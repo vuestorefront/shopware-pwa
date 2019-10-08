6c1746c3dd09a274f9dacd64c2c83ed5e964472a
patzick <tomczyk.patryk@gmail.com>
2019-10-08 09:02:52 +0200

    dx(architecture): prepare package with client

--- packages/shopware-6-client/src/categoryService.ts
+++ packages/shopware-6-client/src/categoryService.ts
@@ -3,7 +3,8 @@ import { config } from "./settings";
 import { CATEGORY_ENDPOINT } from "./endpoints";
 
 const getCategories = async function(): Promise<any> {
-  return axios.get(config.endpoint + CATEGORY_ENDPOINT);
+  const resp = await axios.get(config.endpoint + CATEGORY_ENDPOINT);
+  return resp.data;
 };
 
 export { getCategories };
