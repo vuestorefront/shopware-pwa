d1cc57df5ad5d3f04fe999b85ee37284446319cd
Rafal Makara <rafal.makara@gmail.com>
2019-10-10 22:19:44 +0200

    fix(name): Change display_gross field name to displayGross

--- packages/shopware-6-client/src/interfaces/models/checkout/customer/CustomerGroup.ts
+++ packages/shopware-6-client/src/interfaces/models/checkout/customer/CustomerGroup.ts
@@ -3,6 +3,6 @@ import { CustomField } from "../../common/CustomField";
 export interface CustomerGroup {
   id: string;
   name: string;
-  display_gross: boolean;
+  displayGross: boolean;
   customFields: CustomField[];
 }
