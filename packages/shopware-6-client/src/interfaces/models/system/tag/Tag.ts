331d259ff390c1092e61bb0b23637ab185eae49f
Rafal Makara <rafal.makara@gmail.com>
2019-10-10 20:53:10 +0200

    fix(name): Change TagEntity name to Tag

--- packages/shopware-6-client/src/interfaces/models/system/tag/Tag.ts
+++ packages/shopware-6-client/src/interfaces/models/system/tag/Tag.ts
@@ -5,7 +5,7 @@ import { Order } from "../../checkout/order/Order";
 import { NewsletterRecipient } from "../../content/newsletter/NewsletterRecipient";
 import { ShippingMethod } from "../../checkout/shipping/ShippingMethod";
 
-export interface TagEntity {
+export interface Tag {
   name: string;
   products: Product[] | null;
   media: Media[] | null;
