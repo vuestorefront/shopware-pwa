1f87bdc988ead6771e6a8f80273dbaed9599b7fd
Maciej Kucmus <mkucmus@divante.pl>
2019-10-14 15:05:14 +0200

    feat(product): add unit tests to cover 100% of paramConverter

--- packages/shopware-6-client/src/index.ts
+++ packages/shopware-6-client/src/index.ts
@@ -1,2 +1,4 @@
 export { setup, config } from "./settings";
 export { Category, CategoryService } from "./categoryService";
+export { ProductService } from "./services/productService";
+export { ParamsConverter } from "./helpers/paramsConverter";
