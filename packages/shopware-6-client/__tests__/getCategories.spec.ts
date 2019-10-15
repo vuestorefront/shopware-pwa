2c4710a0110a57291112c0ca6c1ed2483894deee
patzick <tomczyk.patryk@gmail.com>
2019-10-15 13:12:19 +0200

    test(category): tests for getCategories

---
+++ packages/shopware-6-client/__tests__/getCategories.spec.ts
@@ -0,0 +1,15 @@
+import { getCategories } from "@shopware-pwa/shopware-6-client";
+import { apiService } from "../src/apiService";
+
+jest.mock("../src/apiService");
+const mockedAxios = apiService as jest.Mocked<typeof apiService>;
+
+describe("CategoryService - getCategories", () => {
+  it("should return array with categories", async () => {
+    mockedAxios.get.mockResolvedValue({ data: { total: 22 } });
+
+    const result = await getCategories();
+    expect(result.total).toEqual(22);
+    expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+  });
+});
