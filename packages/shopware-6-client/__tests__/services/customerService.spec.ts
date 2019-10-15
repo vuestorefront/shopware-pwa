bde0b53bb02e46659369c8ea8ef0a8045c527d92
Maciej Kucmus <mkucmus@divante.pl>
2019-10-15 10:28:25 +0200

    feat(customer): add register method (WIP)

--- packages/shopware-6-client/__tests__/services/customerService.spec.ts
+++ packages/shopware-6-client/__tests__/services/customerService.spec.ts
@@ -1,11 +1,67 @@
 import { CustomerService } from "../../src/index";
 
+const generateUniqueEmail = (): string => {
+  const currentTime = Date.now();
+  const email = `mkucmus+${currentTime}@divante.com`;
+  return email;
+};
+
+const basicCustomerData = {
+  salutationId:
+    "c370eb5cd1df4d4dbcc78f055b693e79" /** TODO: get the mocked one if possible */,
+  firstName: "Test",
+  lastName: "Test",
+  password: "test123456",
+  email: generateUniqueEmail(),
+  billingAddress: {
+    countryId:
+      "3ff87c8746dc40ecaea1495eb254278e" /** TODO: get the mocked one if possible */,
+    street: "Example 128",
+    zipcode: "555-444",
+    city: "Breslau",
+    phoneNumber: "+48 5543333333"
+  }
+};
+
 const loginCredentials = {
   username: "mkucmus@divante.com",
   password: "test123456"
 };
 
 describe("CustomerService", () => {
+  describe("register", () => {
+    it("should register the new customer with basic data provided", async () => {
+      try {
+        const result = await CustomerService.register(basicCustomerData);
+        expect(result).toHaveProperty("data");
+        expect(result.data).toHaveLength(32);
+      } catch (e) {
+        console.error("Connection problem", e);
+      }
+    });
+    it("should never register a customer without billing address", async () => {
+      try {
+        await CustomerService.register(
+          Object.assign(basicCustomerData, {
+            billingAddress: {}
+          })
+        );
+      } catch (e) {
+        e.response && expect(e.response.status).toEqual(400);
+      }
+    });
+    it("should never register a customer with too short password", async () => {
+      try {
+        await CustomerService.register(
+          Object.assign(basicCustomerData, {
+            password: "short"
+          })
+        );
+      } catch (e) {
+        e.response && expect(e.response.status).toEqual(400);
+      }
+    });
+  });
   describe("login", () => {
     it("should return context token", async () => {
       try {
--- packages/shopware-6-client/src/services/customerService.ts
+++ packages/shopware-6-client/src/services/customerService.ts
@@ -1,14 +1,36 @@
 import axios from "axios";
 import { config } from "../settings";
 import { CUSTOMER_ENDPOINT } from "../endpoints";
+import { BillingAddress } from "../interfaces/models/checkout/customer/BillingAddress";
+import { ShippingAddress } from "../interfaces/models/checkout/customer/ShippingAddress";
 
 interface CustomerLoginParam {
   username: string;
   password: string;
 }
 
+interface CustomerRegisterParam {
+  salutationId: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  email: string;
+  title?: string;
+  birthdayYear?: number;
+  birthdayMonth?: number;
+  birthdayDay?: number;
+  billingAddress: Partial<
+    BillingAddress
+  > /** TODO: replace Partial with correct optional fields in BillingAddress */;
+  shippingAddress?: ShippingAddress;
+}
+
 interface CustomerLoginResponse {
   "sw-context-token": string;
+}
+
+interface CustomerRegisterResponse {
+  data: string;
 }
 
 /**
@@ -20,7 +42,23 @@ interface CustomerLoginResponse {
 export interface CustomerService {
   login: (params: CustomerLoginParam) => Promise<CustomerLoginResponse>;
   logout: (contextToken?: string) => Promise<null>;
+  register: (
+    params: CustomerRegisterParam
+  ) => Promise<CustomerRegisterResponse>;
 }
+
+/**
+ * @description Register a customer
+ */
+const register = async function(
+  params: CustomerRegisterParam
+): Promise<CustomerRegisterResponse> {
+  const resp = await axios.post(
+    `${config.endpoint}${CUSTOMER_ENDPOINT}`,
+    params
+  );
+  return resp.data;
+};
 
 /**
  * @description Get the context token for current user
@@ -56,6 +94,7 @@ const logout = async function(contextToken?: string): Promise<null> {
  * @description Expose public methods of the service
  */
 export const CustomerService: CustomerService = {
+  register,
   login,
   logout
 };
