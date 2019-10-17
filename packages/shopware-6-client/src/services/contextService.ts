4050658cd66921368ff54720e8aba09e220a465a
patzick <tomczyk.patryk@gmail.com>
2019-10-17 19:54:20 +0200

    refactor: common method names

--- packages/shopware-6-client/src/services/contextService.ts
+++ packages/shopware-6-client/src/services/contextService.ts
@@ -16,87 +16,7 @@ import { SearchResult } from "../interfaces/response/SearchResult";
 import { UpdateContextParams } from "../interfaces/request/UpdateContextParams";
 import { UpdateContextResponse } from "../interfaces/response/UpdateContextResponse";
 
-export const getAvailableCurrencies = async function(): Promise<
-  SearchResult<Currency[]>
-> {
-  const resp = await apiService.get(getContextCurrencyEndpoint());
-
-  return resp.data;
-};
-
-export const setCurrentCurrency = async function(
-  contextToken: string | null,
-  newCurrencyID: string
-): Promise<UpdateContextResponse> {
-  let params = { currencyId: newCurrencyID };
-  const resp = await updateContext(contextToken, params);
-
-  return resp;
-};
-
-export const getAvailableLanguages = async function(): Promise<
-  SearchResult<Language[]>
-> {
-  const resp = await apiService.get(getContextLanguageEndpoint());
-
-  return resp.data;
-};
-
-export const setCurrentLanguage = async function(
-  contextToken: string | null,
-  newLanguageId: string
-): Promise<UpdateContextResponse> {
-  let params = { languageId: newLanguageId };
-  const resp = await updateContext(contextToken, params);
-
-  return resp;
-};
-
-export const getAvailableCountries = async function(): Promise<
-  SearchResult<Country[]>
-> {
-  const resp = await apiService.get(getContextCountryEndpoint());
-
-  return resp.data;
-};
-
-export const getAvailablePaymentMethods = async function(): Promise<
-  SearchResult<PaymentMethod[]>
-> {
-  const resp = await apiService.get(getContextPaymentMethodEndpoint());
-
-  return resp.data;
-};
-
-export const setCurrentPaymentMethod = async function(
-  contextToken: string | null,
-  newPaymentMethodId: string
-): Promise<UpdateContextResponse> {
-  let params = { paymentMethodId: newPaymentMethodId };
-  const resp = await updateContext(contextToken, params);
-
-  return resp;
-};
-
-export const getAvailableShippingMethods = async function(): Promise<
-  SearchResult<ShippingMethod[]>
-> {
-  const resp = await apiService.get(getContextShippingMethodEndpoint());
-
-  return resp.data;
-};
-
-export const setCurrentShippingMethod = async function(
-  contextToken: string | null,
-  newShippingMethodId: string
-): Promise<UpdateContextResponse> {
-  let params = { shippingMethodId: newShippingMethodId };
-  const resp = await updateContext(contextToken, params);
-
-  return resp;
-};
-
-const updateContext = async function(
+async function updateContext(
   contextToken: string | null,
   params: UpdateContextParams
 ): Promise<UpdateContextResponse> {
@@ -107,4 +27,84 @@ const updateContext = async function(
     : await apiService.patch(getContextEndpoint(), params, undefined);
 
   return resp.data;
-};
+}
+
+export async function getAvailableCurrencies(): Promise<
+  SearchResult<Currency[]>
+> {
+  const resp = await apiService.get(getContextCurrencyEndpoint());
+
+  return resp.data;
+}
+
+export async function setCurrentCurrency(
+  contextToken: string | null,
+  newCurrencyID: string
+): Promise<UpdateContextResponse> {
+  let params = { currencyId: newCurrencyID };
+  const resp = await updateContext(contextToken, params);
+
+  return resp;
+}
+
+export async function getAvailableLanguages(): Promise<
+  SearchResult<Language[]>
+> {
+  const resp = await apiService.get(getContextLanguageEndpoint());
+
+  return resp.data;
+}
+
+export async function setCurrentLanguage(
+  contextToken: string | null,
+  newLanguageId: string
+): Promise<UpdateContextResponse> {
+  let params = { languageId: newLanguageId };
+  const resp = await updateContext(contextToken, params);
+
+  return resp;
+}
+
+export async function getAvailableCountries(): Promise<
+  SearchResult<Country[]>
+> {
+  const resp = await apiService.get(getContextCountryEndpoint());
+
+  return resp.data;
+}
+
+export async function getAvailablePaymentMethods(): Promise<
+  SearchResult<PaymentMethod[]>
+> {
+  const resp = await apiService.get(getContextPaymentMethodEndpoint());
+
+  return resp.data;
+}
+
+export async function setCurrentPaymentMethod(
+  contextToken: string | null,
+  newPaymentMethodId: string
+): Promise<UpdateContextResponse> {
+  let params = { paymentMethodId: newPaymentMethodId };
+  const resp = await updateContext(contextToken, params);
+
+  return resp;
+}
+
+export async function getAvailableShippingMethods(): Promise<
+  SearchResult<ShippingMethod[]>
+> {
+  const resp = await apiService.get(getContextShippingMethodEndpoint());
+
+  return resp.data;
+}
+
+export async function setCurrentShippingMethod(
+  contextToken: string | null,
+  newShippingMethodId: string
+): Promise<UpdateContextResponse> {
+  let params = { shippingMethodId: newShippingMethodId };
+  const resp = await updateContext(contextToken, params);
+
+  return resp;
+}
