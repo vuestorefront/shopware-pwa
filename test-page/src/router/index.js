6105baf826543701e224a841bdd796938c4d0c2b
patzick <tomczyk.patryk@gmail.com>
2019-10-18 15:46:27 +0200

    chore: add sfui category page

--- test-page/src/router/index.js
+++ test-page/src/router/index.js
@@ -18,6 +18,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/category",
+    name: "category",
+    component: () =>
+      import(/* webpackChunkName: "category" */ "../views/Category.vue")
   }
 ];
 
