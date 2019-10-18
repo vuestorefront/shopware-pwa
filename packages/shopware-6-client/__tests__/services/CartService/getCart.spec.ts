fa4078557af3ec4cb422c3f78b92994e519c2bb8
Rafal Makara <rafal.makara@gmail.com>
2019-10-18 16:16:16 +0200

    fix(test): Fix failing test getCart.spec.ts by fixing the level of 'data' nesting

--- packages/shopware-6-client/__tests__/services/CartService/getCart.spec.ts
+++ packages/shopware-6-client/__tests__/services/CartService/getCart.spec.ts
@@ -1,4 +1,4 @@
-import { getCart } from "@shopware-pwa/shopware-6-client";
+import { getCart, update } from "@shopware-pwa/shopware-6-client";
 import { apiService } from "../../../src/apiService";
 
 jest.mock("../../../src/apiService");
@@ -6,16 +6,19 @@ const mockedAxios = apiService as jest.Mocked<typeof apiService>;
 
 describe("CartService - getCart", () => {
   beforeEach(() => {
+    update({ contextToken: "NWDdcRTTWoPk4Ngv13z5NDMMsDFRb9W6" });
     jest.resetAllMocks();
   });
 
   it("should return existing cart object", async () => {
     mockedAxios.get.mockResolvedValueOnce({
-      name: "3a64e872ca404522a2c5d43ebc751e6b",
-      token: "NWDdcRTTWoPk4Ngv13z5NDMMsDFRb9W6",
-      lineItems: {
-        label: "Red T-Shirt",
-        quantity: 2
+      data: {
+        name: "3a64e872ca404522a2c5d43ebc751e6b",
+        token: "NWDdcRTTWoPk4Ngv13z5NDMMsDFRb9W6",
+        lineItems: {
+          label: "Red T-Shirt",
+          quantity: 2
+        }
       }
     });
 
@@ -24,7 +27,6 @@ describe("CartService - getCart", () => {
     const result = await getCart();
     expect(mockedAxios.get).toBeCalledTimes(1);
     expect(mockedAxios.get).toBeCalledWith("/checkout/cart");
-    expect(result).toHaveProperty("lineItems");
     expect(result.lineItems).not.toBeNull();
     expect(result.name).toEqual(cartName);
   });
