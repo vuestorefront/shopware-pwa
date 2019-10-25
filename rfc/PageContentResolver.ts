f0749e40b856446e9d234a2777af1fbf42035f3a
Rafal Makara <rafal.makara@gmail.com>
2019-10-25 11:21:30 +0200

    feat: Update PageContentResolver version 0.2 for Maciej

--- rfc/PageContentResolver.ts
+++ rfc/PageContentResolver.ts
@@ -1,134 +1,123 @@
+interface PageForHeadless {
+  pageTypeId: Enumerator;
+  pageType: string;
+  data: CategoryPageForHeadless | ProductPageForHeadless;
+}
+
 interface CategoryPageForHeadless {
+  CategoryCmsPage: CmsPage;
+  availableFilters: AvailableFilters;
+  availableSortings: AvailableSortings;
   categories: CategoryForHeadless[];
 }
 
 interface ProductPageForHeadless {
+  ProductCmsPage: CmsPage;
   product: ProductForHeadless;
 }
 
 interface CategoryForHeadless {
-  parentId: string | null;
-  autoIncrement: number;
-  mediaId: string | null;
+  // basic category data
+  id: string;
   name: string | null;
+  description: string;
+  childCount: number;
+  type: string;
+
+  // url to external resource if "category" is not a category but external link
+  externalLink: string | null;
+
+  // can we delete that field and include only visible categories in response?
+  visible: boolean;
+  // can we delete that field and include only active categories in response?
+  active: boolean;
+
+  // structure in a tree
+  parentId: string | null;
   breadcrumb: string[];
   level: number;
-  active: boolean;
-  childCount: number;
-  displayNestedProducts: boolean;
-  parent: Category | null;
-  children: Category[] | null;
-  translations: CategoryTranslation[] | null;
-  media: Media | null;
-  products: Product[] | null;
-  nestedProducts: Product[] | null;
   afterCategoryId: string | null;
-  customFields: CustomField[];
-  tags: Tag[] | null;
+
+  // child categories and products
+  children: Category[] | null;
+  products: Product[] | null;
+
+  // what's that?
+  displayNestedProducts: boolean;
+  nestedProducts: Product[] | null;
+
+  // how to handle cms pages?
   cmsPageId: string | null;
   cmsPage: CmsPage | null;
-  slotConfig: [] | null;
-  externalLink: string | null;
-  visible: boolean;
-  type: string;
-  description: string;
-  id: string;
-  parentVersionId: string;
-  afterCategoryVersionId: string;
+
+  // I guess we do not need media on category level. More like on cms page level?
+  mediaId: string | null;
+  media: Media | null;
 }
 
 interface ProductForCategoryHeadless {
-  calculatedListingPrice: ListingPrice[];
-  calculatedPrices: Price[];
-  calculatedPrice: CalculatedPrice;
-  sortedProperties: PropertyGroup[] | null;
-  isNew: boolean;
-  parentId: string | null;
-  childCount: number;
-  autoIncrement: number;
-  taxId: string | null;
-  manufacturerId: string | null;
-  unitId: string | null;
-  active: boolean;
-  displayGroup: string;
-  price: Price[] | null;
-  manufacturerNumber: string | null;
-  ean: string | null;
-  productNumber: string;
-  stock: number;
-  availableStock: number | null;
-  available: boolean;
-  deliveryTimeId: string | null;
-  deliveryTime: DeliveryTime;
-  restockTime: number;
-  isCloseout: boolean | null;
-  purchaseSteps: number | null;
-  maxPurchase: number | null;
-  minPurchase: number | null;
-  purchaseUnit: number | null;
-  referenceUnit: number | null;
-  shippingFree: boolean | null;
-  purchasePrice: number | null;
-  markAsTopseller: boolean | null;
-  weight: number | null;
-  width: number | null;
-  height: number | null;
-  length: number | null;
-  releaseDate: Date;
-  categoryTree: [] | null;
-  optionsIds: [] | null;
-  propertyIds: [] | null;
-  additionalText: string | null;
+  // basic product data for category page
+  id: string;
   name: string | null;
-  keywords: string | null;
   description: string | null;
-  metaTitle: string | null;
-  packUnit: string | null;
-  tax: Tax;
-  manufacturer: ProductManufacturer | null;
-  unit: Unit | null;
+
+  // can we delete active and get only active products?
+  active: boolean;
+
+  // boolean labels to display
+  available: boolean;
+  isNew: boolean;
+  isCloseout: boolean | null;
+  shippingFree: boolean | null;
+  markAsTopseller: boolean | null;
+
+  // structure in a tree
+  parentId: string | null;
+
+  // whats the difference between calculated and not calculated prices?
+  calculatedListingPrice: ListingPrice[];
+  calculatedPrice: CalculatedPrice;
+  price: Price[] | null;
   prices: ProductPrice[];
   listingPrices: ListingPrice[] | null;
-  cover: ProductMedia;
-  parent: Product;
-  children: Product[];
-  media: ProductMedia[];
-  translations: ProductTranslation[];
-  categories: Category[];
-  tags: Tag[];
-  properties: PropertyGroupOption[] | null;
-  options: PropertyGroupOption[] | null;
-  categoriesRo: Category[] | null;
+
+  // thumbnail
   coverId: string | null;
-  customFields: CustomField[];
-  tagIds: [] | null;
-  productReviews: ProductReview[] | null;
+  cover: ProductMedia;
+
+  // star rating
   ratingAverage: number | null;
-  extensions: [];
-  id: string;
-  parentVersionId: string;
-  productManufacturerVersionId: string;
-  productMediaVersiond: null;
 }
 
 interface ProductForHeadless {
+  id: string;
+  ean: string | null;
+  productNumber: string;
+
+  // whats the difference between calculated and not calculated prices?
   calculatedListingPrice: ListingPrice[];
   calculatedPrices: Price[];
   calculatedPrice: CalculatedPrice;
+  price: Price[] | null;
+
+  // variants handling?
+  parentId: string | null;
+
   sortedProperties: PropertyGroup[] | null;
   isNew: boolean;
-  parentId: string | null;
-  childCount: number;
-  autoIncrement: number;
+
+  // maybe endpoint can simply return an error instead of returning not-active products?
+  active: boolean;
+
   taxId: string | null;
   manufacturerId: string | null;
   unitId: string | null;
-  active: boolean;
+
+  // what's a displayGroup?
   displayGroup: string;
-  price: Price[] | null;
+
   manufacturerNumber: string | null;
-  ean: string | null;
-  productNumber: string;
+
   stock: number;
   availableStock: number | null;
   available: boolean;
@@ -179,7 +168,7 @@ interface ProductForHeadless {
   productReviews: ProductReview[] | null;
   ratingAverage: number | null;
   extensions: [];
-  id: string;
+
   parentVersionId: string;
   productManufacturerVersionId: string;
   productMediaVersiond: null;
