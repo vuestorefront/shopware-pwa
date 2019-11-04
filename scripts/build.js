302a14ffe5700575b26abcbf02e96bc0bac04949
patzick <tomczyk.patryk@gmail.com>
2019-11-04 18:24:18 +0100

    workflow(build): improve build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,14 @@
 /*
 Produces production builds and stitches together d.ts files.
+
 To specify the package to build, simply pass its name and the desired build
 formats to output (defaults to `buildOptions.formats` specified in that package,
 or "esm,cjs"):
+
 ```
 # name supports fuzzy match. will build all packages with name containing "dom":
 yarn build dom
+
 # specify the format to output
 yarn build core --formats cjs
 ```
@@ -13,7 +16,6 @@ yarn build core --formats cjs
 
 const fs = require("fs-extra");
 const path = require("path");
-const zlib = require("zlib");
 const chalk = require("chalk");
 const execa = require("execa");
 const { gzipSync } = require("zlib");
@@ -25,9 +27,14 @@ const targets = args._;
 const formats = args.formats || args.f;
 const devOnly = args.devOnly || args.d;
 const prodOnly = !devOnly && (args.prodOnly || args.p);
+const buildTypes = args.t || args.types;
 const buildAllMatching = args.all || args.a;
+const lean = args.lean || args.l;
 const commit = execa.sync("git", ["rev-parse", "HEAD"]).stdout.slice(0, 7);
-(async () => {
+
+run();
+
+async function run() {
   if (!targets.length) {
     await buildAll(allTargets);
     checkAllSizes(allTargets);
@@ -35,7 +42,7 @@ const commit = execa.sync("git", ["rev-parse", "HEAD"]).stdout.slice(0, 7);
     await buildAll(fuzzyMatchTarget(targets, buildAllMatching));
     checkAllSizes(fuzzyMatchTarget(targets, buildAllMatching));
   }
-})();
+}
 
 async function buildAll(targets) {
   for (const target of targets) {
@@ -52,7 +59,6 @@ async function build(target) {
   const env =
     (pkg.buildOptions && pkg.buildOptions.env) ||
     (devOnly ? "development" : "production");
-
   await execa(
     "rollup",
     [
@@ -63,16 +69,17 @@ async function build(target) {
         `NODE_ENV:${env}`,
         `TARGET:${target}`,
         formats ? `FORMATS:${formats}` : ``,
-        args.types ? `TYPES:true` : ``,
-        prodOnly ? `PROD_ONLY:true` : ``
+        buildTypes ? `TYPES:true` : ``,
+        prodOnly ? `PROD_ONLY:true` : ``,
+        lean ? `LEAN:true` : ``
       ]
-        .filter(_ => _)
+        .filter(Boolean)
         .join(",")
     ],
     { stdio: "inherit" }
   );
 
-  if (args.types && pkg.types) {
+  if (buildTypes && pkg.types) {
     console.log();
     console.log(
       chalk.bold(chalk.yellow(`Rolling up type definitions for ${target}...`))
@@ -91,6 +98,17 @@ async function build(target) {
     });
 
     if (result.succeeded) {
+      // concat additional d.ts to rolled-up dts (mostly for JSX)
+      if (pkg.buildOptions && pkg.buildOptions.dts) {
+        const dtsPath = path.resolve(pkgDir, pkg.types);
+        const existing = await fs.readFile(dtsPath, "utf-8");
+        const toAdd = await Promise.all(
+          pkg.buildOptions.dts.map(file => {
+            return fs.readFile(path.resolve(pkgDir, file), "utf-8");
+          })
+        );
+        await fs.writeFile(dtsPath, existing + "\n" + toAdd.join("\n"));
+      }
       console.log(
         chalk.bold(chalk.green(`API Extractor completed successfully.`))
       );
