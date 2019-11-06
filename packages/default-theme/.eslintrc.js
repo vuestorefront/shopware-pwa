f968b235667b1709f0cf614f3649e970eb2acc50
patzick <tomczyk.patryk@gmail.com>
2019-11-06 13:32:30 +0100

    feat: move category page to nuxt

--- packages/default-theme/.eslintrc.js
+++ packages/default-theme/.eslintrc.js
@@ -14,10 +14,7 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['prettier'],
   // add your custom rules here
-  rules: {
-  }
+  rules: {}
 }
--- packages/default-theme/nuxt.config.js
+++ packages/default-theme/nuxt.config.js
@@ -35,7 +35,7 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    // '@nuxtjs/eslint-module'
   ],
   /*
    ** Nuxt.js modules
