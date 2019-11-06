b502757de4d9aa5f3a56fb1d797dc1942d56cd07
patzick <tomczyk.patryk@gmail.com>
2019-11-06 09:42:18 +0100

    workflow: fix linking dependencies

--- scripts/linkDependencies.js
+++ scripts/linkDependencies.js
@@ -8,13 +8,32 @@ const path = require("path");
 const apiClientDir = path.resolve(__dirname, "../packages/shopware-6-client");
 const defaultThemeDir = path.resolve(__dirname, "../packages/default-theme");
 
+const nuxtPackageCoreDir = path.resolve(
+  __dirname,
+  "../vsf-core-packages/nuxt-module"
+);
+
 async function run() {
+  /**
+   * Link shopware-6-client
+   */
   await execa("yarn", ["link"], {
     stdio: "inherit",
     cwd: apiClientDir
   });
-
   await execa("yarn", ["link", "@shopware-pwa/shopware-6-client"], {
+    stdio: "inherit",
+    cwd: defaultThemeDir
+  });
+
+  /**
+   * link core nuxt-module package
+   */
+  await execa("yarn", ["link"], {
+    stdio: "inherit",
+    cwd: nuxtPackageCoreDir
+  });
+  await execa("yarn", ["link", "@vue-storefront/nuxt"], {
     stdio: "inherit",
     cwd: defaultThemeDir
   });
