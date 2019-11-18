47cca4d974c468b1236d16843484b2e8f9646ee2
patzick <tomczyk.patryk@gmail.com>
2019-11-18 12:30:52 +0100

    chore: linter

--- packages/shopware-6-client/__tests__/services/NavigationService/getNavigation.spec.ts
+++ packages/shopware-6-client/__tests__/services/NavigationService/getNavigation.spec.ts
@@ -10,20 +10,25 @@ describe("NavigationService - getNavigation", () => {
   });
   it("should return navigation elements for given depth and rootNode", async () => {
     mockedAxios.post.mockResolvedValueOnce({
-      data: { count: 1, elements: [
+      data: {
+        count: 1,
+        elements: [
           {
-            "id": "83e555dee2d2425eba42b7c628b65c67",
-            "path": "/navigation/83e555dee2d2425eba42b7c628b65c67",
-            "name": "Music",
-            "children": null,
-            "count": 9,
-            "level": 1,
-            "extensions": []
-          },
-        ] 
+            id: "83e555dee2d2425eba42b7c628b65c67",
+            path: "/navigation/83e555dee2d2425eba42b7c628b65c67",
+            name: "Music",
+            children: null,
+            count: 9,
+            level: 1,
+            extensions: []
+          }
+        ]
       }
     });
-    const result = await getNavigation({depth: 1, rootNode: "5e0bf3a85da746e1ba2f9672910ea361"});
+    const result = await getNavigation({
+      depth: 1,
+      rootNode: "5e0bf3a85da746e1ba2f9672910ea361"
+    });
     expect(mockedAxios.post).toBeCalledTimes(1);
     expect(mockedAxios.post).toBeCalledWith("/vsf/navigation", {
       depth: 1,
