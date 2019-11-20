5770371181c22912b6705306cf3aa3630de2f728
patzick <tomczyk.patryk@gmail.com>
2019-11-20 09:31:00 +0100

    feat(composables): setup composables package

--- packages/composables/index.js
+++ packages/composables/index.js
@@ -1,7 +1,8 @@
-"use strict";
+// "use strict";
 
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./dist/composables.cjs.prod.js");
-} else {
-  module.exports = require("./dist/composables.cjs.js");
-}
+// if (process.env.NODE_ENV === "production") {
+//   module.exports = require("./dist/composables.cjs.prod.js");
+// } else {
+//   module.exports = require("./dist/composables.cjs.js");
+// }
+export * from "./src/index.ts";
