18ced43e9eb70b7dcd6b69dee9af216ab7e604c3
patzick <tomczyk.patryk@gmail.com>
2019-12-05 10:56:59 +0100

    workflow: prepare UI script

---
+++ scripts/prepareSFUIRepoClone.js
@@ -0,0 +1,51 @@
+/**
+ * Setup core dependencies as they're not published yet
+ */
+
+const execa = require("execa");
+const path = require("path");
+const fs = require("fs-extra");
+
+const tempDir = path.resolve(__dirname, "../temp");
+const repoDir = `${tempDir}/storefront-ui`;
+const themeDir = path.resolve(__dirname, "../packages/default-theme");
+
+async function run() {
+  /**
+   * Clone newest Next repo
+   */
+  if (fs.existsSync(repoDir)) {
+    fs.removeSync(repoDir);
+  }
+  await execa(
+    "git",
+    [
+      "clone",
+      "--single-branch",
+      "--branch",
+      "develop",
+      "https://github.com/DivanteLtd/storefront-ui",
+      repoDir
+    ],
+    {
+      stdio: "inherit"
+    }
+  );
+
+  await execa("yarn", [], {
+    stdio: "inherit",
+    cwd: repoDir
+  });
+
+  await execa("npx", ["yalc", "publish"], {
+    stdio: "inherit",
+    cwd: repoDir
+  });
+
+  await execa("npx", ["yalc", "add", "@storeftont-ui/vue"], {
+    stdio: "inherit",
+    cwd: themeDir
+  });
+}
+
+run();
