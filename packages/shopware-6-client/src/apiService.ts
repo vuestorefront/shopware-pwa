46122a367e5a5417aa0f4690d4c4ef38c7730769
patzick <tomczyk.patryk@gmail.com>
2019-12-08 14:47:27 +0100

    test: reload axios config only when has a difference

--- packages/shopware-6-client/src/apiService.ts
+++ packages/shopware-6-client/src/apiService.ts
@@ -1,19 +1,27 @@
 import axios from "axios";
-// import { config } from "./settings";
+import { config } from "./settings";
 import { responseInterceptor, errorInterceptor } from "./apiInterceptors";
 
 export const apiService = axios.create({});
 
 export function reloadConfiguration() {
-  console.error('NOT CHANGING AXIOS CONFIG!')
-  // apiService.defaults.baseURL = config.endpoint;
-  // apiService.defaults.headers.common["sw-access-key"] = config.accessToken;
-  // if (config.contextToken) {
-  //   apiService.defaults.headers.common["sw-context-token"] =
-  //     config.contextToken;
-  // } else {
-  //   delete apiService.defaults.headers.common["sw-context-token"];
-  // }
+  console.error("Try to change axios config!");
+  if (apiService.defaults.baseURL !== config.endpoint)
+    apiService.defaults.baseURL = config.endpoint;
+  if (
+    apiService.defaults.headers.common["sw-access-key"] !== config.accessToken
+  )
+    apiService.defaults.headers.common["sw-access-key"] = config.accessToken;
+  if (
+    config.contextToken &&
+    apiService.defaults.headers.common["sw-context-token"] !==
+      config.contextToken
+  ) {
+    apiService.defaults.headers.common["sw-context-token"] =
+      config.contextToken;
+  } else if (!config.contextToken) {
+    delete apiService.defaults.headers.common["sw-context-token"];
+  }
 }
 reloadConfiguration();
 
