4cf46d2ec05287ba1d85db7b3a02a8c9d769113b
patzick <tomczyk.patryk@gmail.com>
2019-12-08 14:35:47 +0100

    test: not updating axios headers

--- packages/shopware-6-client/src/apiService.ts
+++ packages/shopware-6-client/src/apiService.ts
@@ -1,18 +1,19 @@
 import axios from "axios";
-import { config } from "./settings";
+// import { config } from "./settings";
 import { responseInterceptor, errorInterceptor } from "./apiInterceptors";
 
 export const apiService = axios.create({});
 
 export function reloadConfiguration() {
-  apiService.defaults.baseURL = config.endpoint;
-  apiService.defaults.headers.common["sw-access-key"] = config.accessToken;
-  if (config.contextToken) {
-    apiService.defaults.headers.common["sw-context-token"] =
-      config.contextToken;
-  } else {
-    delete apiService.defaults.headers.common["sw-context-token"];
-  }
+  console.error('NOT CHANGING AXIOS CONFIG!')
+  // apiService.defaults.baseURL = config.endpoint;
+  // apiService.defaults.headers.common["sw-access-key"] = config.accessToken;
+  // if (config.contextToken) {
+  //   apiService.defaults.headers.common["sw-context-token"] =
+  //     config.contextToken;
+  // } else {
+  //   delete apiService.defaults.headers.common["sw-context-token"];
+  // }
 }
 reloadConfiguration();
 
