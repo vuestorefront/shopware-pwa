e926b28d269be959c80182a4017e3c8bba946015
krskibin <krskibin@gmail.com>
2019-12-11 13:54:06 +0100

    refactor(theme): Divide Product View into serval component

---
+++ packages/default-theme/components/cms/elements/SwProductDetails.vue
@@ -0,0 +1,435 @@
+<template>
+  <div class="sw-product-details">
+    <div class="product-details__mobile-top">
+      <div>
+        <SfHeading
+          :title="name"
+          :level="1"
+          class="sf-heading--no-underline sf-heading--left product-details__heading"
+        />
+        <div class="product-details__sub">
+          <SfPrice
+            :regular="`$${price}`"
+            class="sf-price--big product-details__sub-price"
+          />
+          <div class="product-details__sub-rating" v-if="reviews.length">
+            <SfRating :score="ratingAverage" :max="5" />
+            <div class="product-details__sub-reviews desktop-only">
+              Read all {{ reviews.length }} review
+            </div>
+            <div class="product-details__sub-reviews mobile-only">
+              ({{ reviews.length }})
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p class="product-details__description desktop-only">
+      {{ description }}
+    </p>
+    <div class="product-details__action">
+      <button class="sf-action" v-if="sizes.length > 0">Size guide</button>
+    </div>
+    <div class="product-details__section" v-if="isSimple">
+      <SfProperty v-if="color" name="Color" :value="color.name" />
+      <SfProperty v-if="size" name="Size" :value="size.name" />
+    </div>
+    <div class="product-details__section" v-if="hasChildren">
+      <SfSelect
+        v-if="sizes.length"
+        v-model="selectedSize"
+        label="Size"
+        class="sf-select--bordered product-details__attribute"
+      >
+        <SfSelectOption
+          v-for="size in sizes"
+          :key="size.value"
+          :value="size.value"
+        >
+          <SfProductOption :label="size.label" />
+        </SfSelectOption>
+      </SfSelect>
+      <SfSelect
+        v-if="colors.length"
+        v-model="selectedColor"
+        label="Color"
+        class="sf-select--bordered product-details__attribute"
+      >
+        <SfSelectOption
+          v-for="color in colors"
+          :key="color.value"
+          :value="color.value"
+        >
+          <SfProductOption :label="color.label" :color="color.color" />
+        </SfSelectOption>
+      </SfSelect>
+    </div>
+    <div class="product-details__section">
+      <SfAlert
+        message="Low in stock"
+        type="warning"
+        class="product-details__alert mobile-only"
+      />
+      <SfAddToCart
+        :stock="stock"
+        v-model="quantity"
+        class="product-details__add-to-cart"
+        @click="addToCart"
+      />
+      <div class="product-details__action">
+        <button class="sf-action">Save for later</button>
+      </div>
+      <div class="product-details__action">
+        <button class="sf-action">Add to compare</button>
+      </div>
+    </div>
+    <SfTabs class="product-details__tabs" :openTab="1">
+      <SfTab title="Description">
+        <div>
+          <p>
+            {{ product.description }}
+          </p>
+        </div>
+      </SfTab>
+      <SfTab title="Properties">
+        <div class="product-details__properties">
+          <SfProperty
+            v-for="(property, i) in properties"
+            :key="i"
+            :name="property.name"
+            :value="property.value"
+            class="product-details__product-property"
+          />
+        </div>
+      </SfTab>
+      <SfTab title="Read reviews" v-if="reviews.length">
+        <SfReview
+          class="product-details__review"
+          v-for="review in reviews"
+          :key="review.id"
+          :author="review.author"
+          :date="review.date"
+          :message="review.message"
+          :rating="review.rating"
+          :max-rating="5"
+        />
+      </SfTab>
+      <SfTab title="Manufacturer" v-if="product.manufacturer">
+        <SfHeading
+          :title="product.manufacturer.name"
+          :level="3"
+          class="sf-heading--no-underline sf-heading--left"
+        />
+        <p v-if="product.manufacturer.description">
+          {{ product.manufacturer.description }}
+        </p>
+      </SfTab>
+    </SfTabs>
+  </div>
+</template>
+
+<script>
+import {
+  SfProperty,
+  SfHeading,
+  SfPrice,
+  SfRating,
+  SfSelect,
+  SfProductOption,
+  SfAddToCart,
+  SfTabs,
+  SfGallery,
+  SfProductCard,
+  SfCarousel,
+  SfSection,
+  SfImage,
+  SfBanner,
+  SfBottomNavigation,
+  SfCircleIcon,
+  SfIcon,
+  SfAlert,
+  SfSticky,
+  SfReview
+} from '@storefront-ui/vue'
+import { useProduct, useAddToCart } from '@shopware-pwa/composables'
+import {
+  getProductOptions,
+  getProductProperties,
+  getProductOption,
+  getProductReviews,
+  getProductRegularPrice,
+  isProductSimple
+} from '@shopware-pwa/helpers'
+export default {
+  name: 'SwProductDetails',
+  components: {
+    SfAlert,
+    SfProperty,
+    SfHeading,
+    SfPrice,
+    SfRating,
+    SfSelect,
+    SfProductOption,
+    SfAddToCart,
+    SfTabs,
+    SfGallery,
+    SfProductCard,
+    SfCarousel,
+    SfSection,
+    SfBanner,
+    SfBottomNavigation,
+    SfCircleIcon,
+    SfIcon,
+    SfSticky,
+    SfReview
+  },
+  props: {
+    product: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  setup({ page }) {
+    const { addToCart, quantity } = useAddToCart(page && page.product)
+
+    return {
+      quantity,
+      addToCart
+    }
+  },
+  computed: {
+    price() {
+      return getProductRegularPrice({ product: this.product })
+    },
+    name() {
+      return this.product && this.product.name
+    },
+    description() {
+      return this.product && this.product.description
+    },
+    ratingAverage() {
+      return this.product && this.product.ratingAverage
+    },
+    hasChildren() {
+      return this.product && this.product.childCount > 0
+    },
+    isSimple() {
+      return isProductSimple({ product: this.product })
+    },
+    properties() {
+      return getProductProperties({ product: this.product })
+    },
+    color() {
+      if (!this.isSimple) {
+        return ''
+      }
+      return getProductOption({
+        product: this.product,
+        attribute: 'color'
+      })
+    },
+    size() {
+      if (!this.isSimple) {
+        return ''
+      }
+      return getProductOption({
+        product: this.product,
+        attribute: 'size'
+      })
+    },
+    colors() {
+      return getProductOptions({
+        product: this.product,
+        attribute: 'color'
+      })
+    },
+    sizes() {
+      return getProductOptions({
+        product: this.product,
+        attribute: 'size'
+      })
+    },
+    reviews() {
+      return getProductReviews({ product: this.product })
+    },
+    stock() {
+      return this.product && this.product.stock
+    }
+  },
+
+  methods: {
+    toggleWishlist(index) {
+      this.products[index].isOnWishlist = !this.products[index].isOnWishlist
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@storefront-ui/shared/styles/variables';
+@import '~@storefront-ui/shared/styles/helpers/visibility';
+@import '~@storefront-ui/vue/src/utilities/transitions/transitions.scss';
+
+@mixin for-desktop {
+  @media screen and (min-width: $desktop-min) {
+    @content;
+  }
+}
+@mixin for-iOS {
+  @supports (-webkit-overflow-scrolling: touch) {
+    @content;
+  }
+}
+
+.product-details {
+  &__action {
+    display: flex;
+    margin: $spacer-big 0 ($spacer-big / 2);
+    @include for-desktop {
+      justify-content: flex-end;
+    }
+  }
+  &__add-to-cart {
+    margin-top: 1.5rem;
+    @include for-desktop {
+      margin-top: $spacer-extra-big;
+    }
+  }
+  &__alert {
+    margin-top: 1.5rem;
+  }
+  &__attribute {
+    margin-bottom: $spacer-big;
+  }
+  &__description {
+    margin: $spacer-extra-big 0 ($spacer-big * 3) 0;
+    font-family: $body-font-family-secondary;
+    font-size: $font-size-regular-mobile;
+    line-height: 1.6;
+    @include for-desktop {
+      font-size: $font-size-regular-desktop;
+    }
+  }
+  &__divider {
+    margin-top: 30px;
+  }
+  &__heading {
+    margin-top: $spacer-big;
+    ::v-deep .sf-heading__title {
+      font-size: $font-size-big-mobile;
+      font-weight: $body-font-weight-primary;
+      @include for-desktop {
+        font-size: $h1-font-size-desktop;
+        font-weight: $body-font-weight-secondary;
+      }
+    }
+    @include for-desktop {
+      margin-top: 0;
+    }
+  }
+  &__mobile-bar {
+    display: none;
+    padding: $spacer-medium 0;
+    box-sizing: border-box;
+    .product--is-active & {
+      display: block;
+      @include for-desktop {
+        display: none;
+      }
+    }
+    @include for-desktop {
+      display: none;
+    }
+  }
+  &__mobile-top {
+    display: flex;
+    align-items: center;
+    @include for-desktop {
+      display: block;
+    }
+  }
+  &__properties {
+    margin-top: $spacer-big;
+  }
+  &__sub {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+  &__sub-price {
+    flex-basis: 100%;
+    margin-top: $spacer-big / 4;
+    @include for-desktop {
+      flex-basis: auto;
+      margin-top: $spacer-big / 2;
+    }
+  }
+  &__sub-rating {
+    display: flex;
+    margin-top: $spacer-big / 2;
+    @include for-desktop {
+      margin-left: auto;
+    }
+  }
+  &__sub-reviews {
+    margin-left: 10px;
+    font-size: 0.75rem;
+  }
+  &__section {
+    border-bottom: 1px solid #f1f2f3;
+    padding-bottom: 10px;
+    @include for-desktop {
+      border: 0;
+      padding-bottom: 0;
+    }
+    @media (max-width: $desktop-min) {
+      padding-left: $spacer-big;
+      padding-right: $spacer-big;
+    }
+  }
+  &__tabs {
+    margin-top: $spacer-big;
+    @include for-desktop {
+      margin-top: 5 * $spacer-big;
+    }
+    p {
+      margin: 0;
+    }
+  }
+  &__review {
+    padding-bottom: $spacer-big;
+    @include for-desktop {
+      padding-bottom: $spacer-extra-big;
+      border-bottom: 1px solid $c-primary;
+    }
+    & + & {
+      padding-top: $spacer-extra-big;
+      border-top: 1px solid $c-primary;
+      @include for-desktop {
+        border-top: 0;
+        padding-top: $spacer-extra-big;
+      }
+    }
+  }
+  &__product-property {
+    padding: $spacer-small 0;
+  }
+}
+
+.sf-action {
+  padding: 0;
+  border: 0;
+  outline: none;
+  background-color: transparent;
+  color: $c-text;
+  font-family: $body-font-family-secondary;
+  font-size: $font-size-regular-mobile;
+  font-weight: $body-font-weight-secondary;
+  line-height: 1.6;
+  text-decoration: underline;
+  cursor: pointer;
+  @include for-desktop {
+    font-size: $font-size-regular-desktop;
+  }
+}
+</style>
