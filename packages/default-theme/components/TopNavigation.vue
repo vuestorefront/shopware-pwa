abad06195281eb93cac0fa2cf99c7028a15c83a6
Krystian <krskibin@gmail.com>
2019-12-11 19:26:10 +0100

    feat: move cart button to top navigation (#193)
    * feat: move cart button to top navigation
    
    * fix: cart button badge scaling

--- packages/default-theme/components/TopNavigation.vue
+++ packages/default-theme/components/TopNavigation.vue
@@ -1,65 +1,87 @@
 <template>
-  <div>
-    <SfHeader
-      title="Storefront UI"
-      logo="/img/logo.svg"
-      active-sidebar="activeSidebar"
-    >
-      <template #navigation>
-        <nuxt-link to="/"
-          ><SfHeaderNavigationItem>Home</SfHeaderNavigationItem></nuxt-link
-        >
-        <nuxt-link
-          v-for="element in navigationElements"
-          :key="element.id"
-          :to="convertToSlug(element.name)"
-        >
-          <SfHeaderNavigationItem>{{ element.name }}</SfHeaderNavigationItem>
-        </nuxt-link>
-      </template>
+  <div class="top-navigation">
+    <slot v-bind="{navigationElements, activeSidebar, activeIcon}">
+      <SfHeader
+        title="Shopware PWA"
+        logo="/img/logo.svg"
+        active-sidebar="activeSidebar"
+      >
+        <template #navigation>
+          <nuxt-link to="/"><SfHeaderNavigationItem>Home</SfHeaderNavigationItem></nuxt-link>
+          <nuxt-link v-for="element in navigationElements" :key="element.id" :to="convertToSlug(element.name)">
+            <SfHeaderNavigationItem>{{ element.name }}</SfHeaderNavigationItem>
+          </nuxt-link>
+        </template>
 
-      <template #header-icons="{accountIcon, wishlistIcon, cartIcon}">
-        <SfCircleIcon
-          v-if="accountIcon"
-          :icon="accountIcon"
-          class="sf-header__icon"
-          :class="{ 'sf-header__icon--is-active': isLoggedIn }"
-          role="button"
-          aria-label="account"
-          @click="userIconClick"
-        />
-        <SfCircleIcon
-          v-if="wishlistIcon"
-          :icon="wishlistIcon"
-          class="sf-header__icon"
-          :class="{ 'sf-header__icon--is-active': activeIcon === 'wishlist' }"
-          role="button"
-          aria-label="wishlist"
-          :aria-pressed="activeIcon === 'wishlist' ? 'true' : 'false'"
-          @click="$emit('click:wishlist')"
-        />
-      </template>
-    </SfHeader>
-    <SwLoginModal :is-open="isModalOpen" @close="isModalOpen = false" />
+        <template #header-icons="{accountIcon, wishlistIcon, cartIcon}">
+          <div class="sf-header__icons">
+            <SfCircleIcon
+              v-if="accountIcon"
+              :icon="accountIcon"
+              icon-size="20px"
+              class="sf-header__icon"
+              :class="{'sf-header__icon--is-active' : isLoggedIn}"
+              role="button"
+              aria-label="account"
+              :aria-pressed="activeIcon === 'account' ? 'true' :'false'"
+              @click="userIconClick"
+            />
+            <SfCircleIcon
+              v-if="wishlistIcon"
+              :icon="wishlistIcon"
+              icon-size="20px"
+              class="sf-header__icon"
+              :class="{'sf-header__icon--is-active' : activeIcon === 'wishlist'}"
+              role="button"
+              aria-label="wishlist"
+              :aria-pressed="activeIcon === 'wishlist' ? 'true' :'false'"
+              @click="$emit('click:wishlist')"
+            />
+
+            <div class='top-navigation__header-icon header-icons__cart cart-icon'>
+              <SfCircleIcon
+                  v-if="cartIcon"
+                  :icon="cartIcon"
+                  icon-size="20px"
+                  class="sf-header__icon"
+                  :class="{'sf-header__icon--is-active' : activeIcon === 'cart'}"
+                  role="button"
+                  aria-label="cart"
+                  :aria-pressed="activeIcon === 'cart' ? 'true' :'false'"
+                  @click="toggle"
+              />
+              <SfBadge class="cart-icon__badge" v-show="count > 0">{{ count }}</SfBadge>
+            </div>
+          </div>
+        </template>
+      </SfHeader>
+      <SwLoginModal :is-open="isModalOpen" @close="isModalOpen = false" />
+    </slot>
   </div>
 </template>
+
 <script>
 import slugify from 'slugify' // TODO: remove after the navigation is fully implemented
-import { getNavigation, getPage } from '@shopware-pwa/shopware-6-client'
-import { SfHeader, SfCircleIcon } from '@storefront-ui/vue'
+import { getNavigation, getPage  } from '@shopware-pwa/shopware-6-client'
+import { SfHeader, SfCircleIcon, SfBadge } from '@storefront-ui/vue'
+import { useUser, useCart, useCartSidebar } from '@shopware-pwa/composables'
 import SwLoginModal from './modals/SwLoginModal'
-import { useUser } from '@shopware-pwa/composables'
 
 export default {
-  components: { SfHeader, SfCircleIcon, SwLoginModal },
+  components: { SfHeader, SfCircleIcon, SfBadge,SwLoginModal },
   setup() {
     const { isLoggedIn, logout } = useUser()
+    const { count }  = useCart()
+    const { toggle, isOpen } = useCartSidebar()
     return {
+      isOpen,
+      count,
+      toggle,
       isLoggedIn,
       logout
     }
   },
-  data() {
+  data () {
     return {
       navigationElements: [],
       activeSidebar: 'account',
@@ -88,19 +110,25 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
 @import '~@storefront-ui/vue/styles.scss';
 @import '~@storefront-ui/shared/styles/helpers/visibility';
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.top-navigation {
+  .cart-icon {
+    position: relative;
+    display: flex;
 
-    &.nuxt-link-exact-active {
-      color: #42b983;
+    &__badge {
+      position: absolute;
+      bottom: 2.2em;
+      left: 4.5em;
+      font-size: 0.6em;
+      padding: 0.3em 0;
+      border-radius: 100%;
+      width: 2.2em;
     }
   }
 }
+
 </style>
--- packages/default-theme/store/index.js
+++ packages/default-theme/store/index.js
@@ -1,7 +1,8 @@
 export const state = () => ({
   cart: null,
   page: null,
-  user: null
+  user: null,
+  cartSidebarOpen: false
 })
 
 export const mutations = {
@@ -11,13 +12,17 @@ export const mutations = {
   SET_PAGE(state, page) {
     state.page = page
   },
-  SET_USER(state, user) {
+	SET_USER(state, user) {
     state.user = user
+  },
+  SET_CART_SIDEBAR_IS_OPEN(state, flag) {
+    state.cartSidebarOpen = flag
   }
 }
 
 export const getters = {
   getCart: (state) => state.cart,
   getPage: (state) => state.page,
-  getUser: (state) => state.user
+  getUser: (state) => state.user,
+  getIsCartSidebarOpen: (state) => state.cartSidebarOpen
 }
