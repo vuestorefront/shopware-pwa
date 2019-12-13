5a54ce640c086b14e890bcaf3b6e64583a0f19b3
krskibin <krskibin@gmail.com>
2019-12-13 11:54:45 +0100

    fix(theme): change special price type to [Number, String]

--- packages/default-theme/components/cms/elements/SwProductHeading.vue
+++ packages/default-theme/components/cms/elements/SwProductHeading.vue
@@ -1,30 +1,37 @@
 <template>
   <div class="sw-product-heading">
-    <SfHeading
-      :title="name"
-      :level="1"
-      class="sf-heading--no-underline sf-heading--left"
-    />
-    <div class="product-details__sub">
-      <SfPrice
-        :regular="`$${price}`"
-        :special="special"
-        class="sf-price--big product-details__sub-price"
+    <slot name="heading" v-bind="name">
+      <SfHeading
+        :title="name"
+        :level="1"
+        class="sf-heading--no-underline sf-heading--left"
       />
-      <div v-if="reviews.length" class="product-details__sub-rating">
-        <SfRating :score="ratingAverage" :max="5" />
-        <div class="product-details__sub-reviews desktop-only">
-          Read all {{ reviews.length }} review
+    </slot>
+    <div class="product-details__sub">
+      <slot name="price" v-bind="{ price, special }">
+        <SfPrice
+          :regular="`$${price}`"
+          :special="special"
+          class="sf-price--big product-details__sub-price"
+        />
+      </slot>
+      <slot name="reviews" v-bind="reviews">
+        <div v-if="reviews.length" class="product-details__sub-rating">
+          <SfRating :score="ratingAverage" :max="5" />
+          <div class="product-details__sub-reviews desktop-only">
+            Read all {{ reviews.length }} review
+          </div>
+          <div class="product-details__sub-reviews mobile-only">
+            ({{ reviews.length }})
+          </div>
         </div>
-        <div class="product-details__sub-reviews mobile-only">
-          ({{ reviews.length }})
-        </div>
-      </div>
+      </slot>
     </div>
   </div>
 </template>
 <script>
 import { SfHeading, SfPrice, SfRating } from '@storefront-ui/vue'
+
 export default {
   name: 'SwProductHeading',
   components: { SfHeading, SfPrice, SfRating },
@@ -38,7 +45,7 @@ export default {
       default: 0
     },
     special: {
-      type: Number,
+      type: [Number, String],
       default: 0
     },
     ratingAverage: {
