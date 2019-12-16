f0d8aad136c6b855159a81db304ddb4272bfd0c5
patzick <tomczyk.patryk@gmail.com>
2019-12-16 12:32:48 +0100

    refactor: temporary turn off seoURLs

--- packages/helpers/src/product/getProductUrl.ts
+++ packages/helpers/src/product/getProductUrl.ts
@@ -2,9 +2,10 @@ import { Product } from "@shopware-pwa/shopware-6-client";
 
 export function getProductUrl(product: Product | null): string {
   if (!product) return "/";
-  const seoUrl =
-    product.seoUrls &&
-    product.seoUrls.length &&
-    product.seoUrls[0].seoPathInfo;
+  const seoUrl = null; // TODO change after fixing URL resolver
+  // const seoUrl =
+  //   product.seoUrls &&
+  //   product.seoUrls.length &&
+  //   product.seoUrls[0].seoPathInfo;
   return seoUrl ? `/${seoUrl}` : `/detail/${product.id}`;
 }
