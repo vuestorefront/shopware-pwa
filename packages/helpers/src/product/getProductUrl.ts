2ad9cad48b494b45ec3dad964b76f35ad8267e05
patzick <tomczyk.patryk@gmail.com>
2019-12-16 15:05:00 +0100

    chore: update product url without SEO

--- packages/helpers/src/product/getProductUrl.ts
+++ packages/helpers/src/product/getProductUrl.ts
@@ -2,10 +2,11 @@ import { Product } from "@shopware-pwa/shopware-6-client";
 
 export function getProductUrl(product: Product | null): string {
   if (!product) return "/";
-  const seoUrl = null; // TODO change after fixing URL resolver
+  // TODO change after fixing URL resolver
   // const seoUrl =
   //   product.seoUrls &&
   //   product.seoUrls.length &&
   //   product.seoUrls[0].seoPathInfo;
-  return seoUrl ? `/${seoUrl}` : `/detail/${product.id}`;
+  // return seoUrl ? `/${seoUrl}` : `/detail/${product.id}`;
+  return `/detail/${product.id}`;
 }
