db2d69005dbba634f5e7727815e2b276dd71567c
patzick <tomczyk.patryk@gmail.com>
2020-01-02 16:13:34 +0100

    workflow(packages): cjs package file from dist

--- packages/shopware-6-client/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./dist/shopware-6-client.cjs.prod.js");
-} else {
-  module.exports = require("./dist/shopware-6-client.cjs.js");
-}
