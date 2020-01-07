2a99f98552676bf84336467c41a9441f35b09077
Maciej Kucmus <mkucmus@divante.pl>
2020-01-07 16:03:25 +0100

    test: add missing tests

--- packages/helpers/src/urlHelpers.ts
+++ packages/helpers/src/urlHelpers.ts
@@ -1,17 +1,26 @@
 import { SearchCriteria } from "@shopware-pwa/shopware-6-client";
 import queryString from "query-string";
+import { isObject } from "util";
 
 export function parseUrlQuery(query: any): SearchCriteria {
   const searchCriteria: any = {};
-  if (query) {
-    Object.keys(query).forEach((key: string) => {
-      searchCriteria[key] = JSON.parse(query[key]);
-    });
+  if (!query || !isObject(query)) {
+    return searchCriteria;
   }
+  console.warn("TEST 123", query);
+  Object.keys(query).forEach((key: string) => {
+    searchCriteria[key] = JSON.parse(query[key]);
+  });
+
   return searchCriteria;
 }
 
-export function exportUrlQuery(searchCriteria: SearchCriteria): string {
+export function exportUrlQuery(
+  searchCriteria: SearchCriteria
+): string | undefined {
+  if (!searchCriteria) {
+    return;
+  }
   const sC: any = searchCriteria;
   const query: any = {};
   Object.keys(searchCriteria).forEach((key: string) => {
