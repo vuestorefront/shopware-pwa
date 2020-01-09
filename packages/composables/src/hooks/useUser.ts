c05cdf8cb03e4e5a594a4bee7f1ff60d528b5bd8
Maciej Kucmus <mkucmus@divante.pl>
2020-01-09 22:43:09 +0100

    --wip-- customer: load orders, show order list

--- packages/composables/src/hooks/useUser.ts
+++ packages/composables/src/hooks/useUser.ts
@@ -2,10 +2,13 @@ import { ref, Ref, computed } from "@vue/composition-api";
 import {
   login as apiLogin,
   logout as apiLogout,
-  getCustomer
+  getCustomer,
+  getCustomerOrders,
+  
 } from "@shopware-pwa/shopware-6-client";
 import { Customer } from "packages/shopware-6-client/src/interfaces/models/checkout/customer/Customer";
 import { getStore } from "@shopware-pwa/composables";
+import { Order } from "@shopware-pwa/shopware-6-client/src/interfaces/models/checkout/order/Order";
 
 interface UseUser {
   login: ({
@@ -16,17 +19,20 @@ interface UseUser {
     password?: string;
   }) => Promise<boolean>;
   user: Ref<Customer | null>;
+  orders: Ref<Order[] | null>;
   loading: Ref<boolean>;
   error: Ref<any>;
   isLoggedIn: Ref<boolean>;
   refreshUser: () => Promise<void>;
   logout: () => Promise<void>;
+  loadOrders: () => Promise<void>;
 }
 
 export const useUser = (): UseUser => {
   let vuexStore = getStore();
   const loading: Ref<boolean> = ref(false);
   const error: Ref<any> = ref(null);
+  const orders: Ref<Order[] | null> = ref(null)
 
   const user: any = computed(() => {
     return vuexStore.getters.getUser;
@@ -65,6 +71,13 @@ export const useUser = (): UseUser => {
     vuexStore.commit("SET_USER", user);
   };
 
+  const loadOrders = async (): Promise<void> => {
+    loading.value = true
+    const fetchedOrders = await getCustomerOrders();
+    orders.value = fetchedOrders;
+    loading.value = false
+  }
+
   const isLoggedIn = computed(() => !!user.value);
 
   return {
@@ -74,6 +87,8 @@ export const useUser = (): UseUser => {
     loading,
     isLoggedIn,
     refreshUser,
-    logout
+    logout,
+    orders,
+    loadOrders
   };
 };
---
+++ packages/default-theme/components/account/MyProfile.vue
@@ -0,0 +1,49 @@
+<template>
+  <SfTabs :open-tab="1">
+    <SfTab title="Personal data">
+      <SfList>
+        <SfListItem>customerNumber: {{ user.customerNumber }}</SfListItem>
+        <SfListItem>email: {{ user.email }}</SfListItem>
+        <SfListItem>firstname: {{ user.firstName }} </SfListItem>
+        <SfListItem>lastname: {{ user.lastName }} </SfListItem>
+      </SfList>
+    </SfTab>
+
+  </SfTabs>
+  
+</template>
+<script>
+
+import { SfIcon, SfModal, SfInput, SfButton, SfLoader, SfAlert, SfTabs, SfList } from '@storefront-ui/vue'
+import { VuelidateMixin } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+import { useUser } from '@shopware-pwa/composables'
+
+export default {
+  name: "MyProfile",
+  mixins: [VuelidateMixin],
+  components: {SfTabs, SfList},
+  props: {
+  },
+  setup() {
+    const { user } = useUser()
+    return {
+      user
+    }
+  },
+  data() {
+    return {
+      
+    }
+  },
+  
+  methods: {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@storefront-ui/vue/styles.scss';
+@import '~@storefront-ui/shared/styles/helpers/visibility';
+</style>
