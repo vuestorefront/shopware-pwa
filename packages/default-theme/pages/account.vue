06479473279709a80a02ddabc2390dfaadcb3bbf
Maciej Kucmus <mkucmus@divante.pl>
2020-01-09 16:03:45 +0100

    --wip-- login and account page drafts, auth middleware

---
+++ packages/default-theme/pages/account.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>My account</h1>
+
+  </div>
+</template>
+<script>
+
+
+export default {
+  name: 'Account',
+  components: {
+  },
+  middleware: "auth",
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import '~@storefront-ui/vue/styles.scss';
+@import '~@storefront-ui/shared/styles/helpers/visibility';
+
+</style>
---
+++ packages/default-theme/pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <SwLoginModal :is-open="true"/>
+</template>
+<script>
+import SwLoginModal from "../components/modals/SwLoginModal"
+
+export default {
+  name: 'Login',
+  components: {
+    SwLoginModal
+  },
+  middleware: "auth",
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import '~@storefront-ui/vue/styles.scss';
+@import '~@storefront-ui/shared/styles/helpers/visibility';
+
+</style>
