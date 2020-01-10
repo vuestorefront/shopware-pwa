70c1cd88f3d934f9c8cd01a1053021507f8baa79
patzick <tomczyk.patryk@gmail.com>
2020-01-10 17:31:14 +0100

    chore: extract changing property method

--- packages/shopware-6-client/__e2e__/PageService/getPage.e2e.spec.ts
+++ packages/shopware-6-client/__e2e__/PageService/getPage.e2e.spec.ts
@@ -1,29 +1,21 @@
 import { getPage } from "@shopware-pwa/shopware-6-client";
-
-function deepChangeProperty(obj: any, property: string, value: any) {
-  for (var prop in obj) {
-    if (obj[prop] === Object(obj[prop]))
-      deepChangeProperty(obj[prop], property, value);
-    else if (prop === property) obj[prop] = value;
-  }
-}
-
+import { deepChangeProperty } from "../helpers";
 describe("shopware-6-client - E2E - PageService - getPage", () => {
   it("should test / page response", async () => {
     const result = await getPage("");
-    deepChangeProperty(result, "availableStock", "mockedValue");
+    deepChangeProperty(result, "availableStock");
     expect(result).toMatchSnapshot();
   });
 
   it("should test product page response", async () => {
     const result = await getPage("/detail/9cce06f9dc424844989a06cfe3dc98da");
-    deepChangeProperty(result, "availableStock", "mockedValue");
+    deepChangeProperty(result, "availableStock");
     expect(result).toMatchSnapshot();
   });
 
   it("should test category page response", async () => {
     const result = await getPage("Clothing");
-    deepChangeProperty(result, "availableStock", "mockedValue");
+    deepChangeProperty(result, "availableStock");
     expect(result).toMatchSnapshot();
   });
 });
