6ebdf0c3a62a3f1729e5cd0b8f7004ff897233a4
Maciej Kucmus <mkucmus@divante.pl>
2020-01-10 08:25:49 +0100

    --wip-- improvements, redirect on success

--- packages/default-theme/pages/login.vue
+++ packages/default-theme/pages/login.vue
@@ -1,9 +1,5 @@
 <template>
-<<<<<<< 84300017004a397ffa02f09d49d7992e3e1bd6db
   <SwLoginModal :is-open="true" :onClose="redirectToHome" :onSuccess="redirectToMyAccount"/>
-=======
-  <SwLoginModal :is-open="true" :onClose="redirectToHome"/>
->>>>>>> --wip-- handle login, logout and redirect to my account page
 </template>
 <script>
 import SwLoginModal from "../components/modals/SwLoginModal"
@@ -23,12 +19,9 @@ export default {
   methods: {
     redirectToHome() {
       this.$router.push("/")  
-<<<<<<< 84300017004a397ffa02f09d49d7992e3e1bd6db
     },
     redirectToMyAccount() {
       this.$router.push("/account")
-=======
->>>>>>> --wip-- handle login, logout and redirect to my account page
     }
   }
 }
