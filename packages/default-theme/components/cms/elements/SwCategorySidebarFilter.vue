1b5f942925fe0bdbd2ffef09f8afb9d1f6aebaed
mkucmus <maciejkucmus@gmail.com>
2020-01-21 12:57:45 +0100

    refactor(theme): improvements (#308)
    * feat(theme): improvements
    
    * fix: remove unused method
    
    Co-authored-by: Patryk Tomczyk <13100280+patzick@users.noreply.github.com>

--- packages/default-theme/components/cms/elements/SwCategorySidebarFilter.vue
+++ packages/default-theme/components/cms/elements/SwCategorySidebarFilter.vue
@@ -69,16 +69,18 @@
         <div class="filters">
           <div v-for="filter in filters" :key="filter.name">
             <h3 class="filters__title">{{ filter.name }}</h3>
-            <SfFilter
-              v-for="option in filter.options"
-              :key="option.value"
-              :label="option.label"
-              :count="option.count"
-              :color="option.color ? option.color : null"
-              :selected="selectedFilters[filter.name] && !!selectedFilters[filter.name].find((propertyId) => propertyId === option.value)"
-              @click.native="toggleFilter({type: 'equals', value: option.value, field: filter.name})"
-              class="filters__item"
-            />
+            <div v-if="filter && filter.options && filter.options.length">
+              <SfFilter
+                v-for="option in filter.options"
+                :key="option.value"
+                :label="option.label"
+                :count="option.count"
+                :color="option.color ? option.color : null"
+                :selected="selectedFilters[filter.name] && !!selectedFilters[filter.name].find((propertyId) => propertyId === option.value)"
+                @click.native="toggleFilter({type: 'equals', value: option.value, field: filter.name})"
+                class="filters__item"
+              />
+            </div>
           </div>
 
           <div class="filters__buttons">
