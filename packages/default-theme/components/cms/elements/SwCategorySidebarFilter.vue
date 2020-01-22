218de31ab4797b5a76a3217ac7385f10c230a4f3
krskibin <chrisski96@gmail.com>
2020-01-22 16:55:34 +0100

    fix side navbar styles

--- packages/default-theme/components/cms/elements/SwCategorySidebarFilter.vue
+++ packages/default-theme/components/cms/elements/SwCategorySidebarFilter.vue
@@ -215,7 +215,7 @@ export default {
   }
 
   &__main {
-    margin-left: 20px;
+    margin: 0 20px 0 20px;
     flex: 1;
     display: flex;
     align-items: center;
@@ -264,6 +264,7 @@ export default {
   }
   &__counter {
     margin: auto;
+    padding-right: 20px;
     @include for-desktop {
       margin-right: 0;
     }
