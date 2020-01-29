3e022ac1e1f72ec4647b631e73844ac5578b2322
krskibin <krskibin@gmail.com>
2020-01-29 14:24:56 +0100

    test: fix tests

--- packages/composables/src/hooks/useCms/index.ts
+++ packages/composables/src/hooks/useCms/index.ts
@@ -42,7 +42,7 @@ export const useCms = (): any => {
       const result = await getPage(path, searchCriteria);
       vuexStore.commit("SET_PAGE", result);
     } catch (e) {
-      error.value = e;
+      error.value = e.message;
       console.error("Problem with fetching CMS data", e.message);
     } finally {
       loading.value = false;
