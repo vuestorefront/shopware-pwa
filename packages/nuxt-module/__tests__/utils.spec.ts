f2f62ea69f4e199353906b96ee789852faec8ac1
Maciej Kucmus <maciejkucmus@gmail.com>
2020-03-03 13:54:32 +0100

    tests: cover extendComponents method (draft)

--- packages/nuxt-module/__tests__/utils.spec.ts
+++ packages/nuxt-module/__tests__/utils.spec.ts
@@ -1,12 +1,26 @@
-import { addThemeLayouts } from "@shopware-pwa/nuxt-module/src/utils";
+import { addThemeLayouts, extendComponents } from "@shopware-pwa/nuxt-module/src/utils";
 
 describe("nuxt-module - utils", () => {
-    describe("addThemeLayouts", () => {
-      it("adds available local layouts", () => {
-        jest.mock('./testModule');
-        const mockedModuleObject = require('./testModule');
-        addThemeLayouts(mockedModuleObject);
-        expect(mockedModuleObject.addLayout).toBeCalledWith({"src": __dirname + "/node_modules/@shopware-pwa/default-theme/layouts/test.vue"}, "test")
-      })
+  beforeEach(() => {
+    // mock vuex store
+    jest.resetAllMocks();
+  });
+
+  describe("addThemeLayouts", () => {
+    it("adds available local layouts", () => {
+      jest.mock('./testModule');
+      const mockedModuleObject = require('./testModule');
+      addThemeLayouts(mockedModuleObject);
+      expect(mockedModuleObject.addLayout).toBeCalledWith({"src": __dirname + "/node_modules/@shopware-pwa/default-theme/layouts/test.vue"}, "test")
     })
+  })
+
+  describe("extendComponents", () => {
+    it("replace files for given aliases", () => {
+      jest.mock('./testModule');
+      const mockedModuleObject = require('./testModule');
+      extendComponents(mockedModuleObject);
+      expect(mockedModuleObject.extendBuild).toBeCalledTimes(1)
+    })
+  })
 });
