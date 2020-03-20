d92c747ab3905478d2581b60f65bb32448555a42
Michal-Dziedzinski <michal.marcin.dziedzinski@gmail.com>
2020-03-20 10:10:53 +0100

    fix: fix computed properties in register component

--- packages/default-theme/components/SwRegister.vue
+++ packages/default-theme/components/SwRegister.vue
@@ -194,11 +194,11 @@ export default {
       }
     },
     getErrorMessage() {
-      if (userError)
+      if (this.userError)
         return 'Cannot create a new account, the user may already exist'
-      if (salutationsError)
+      if (this.salutationsError)
         return "Couldn't fetch available salutations, please contact the administration."
-      if (countriesError)
+      if (this.countriesError)
         return "Couldn't fetch available countries, please contact the administration."
     }
   },
