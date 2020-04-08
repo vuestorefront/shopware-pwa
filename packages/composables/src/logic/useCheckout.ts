8e1719c049f6854f510518062795ea5242446f69
patzick <tomczyk.patryk@gmail.com>
2020-04-08 13:26:53 +0200

    feat: useCheckout with tests

--- packages/composables/src/logic/useCheckout.ts
+++ packages/composables/src/logic/useCheckout.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import { Ref, computed } from "@vue/composition-api";
-import { useUser } from "@shopware-pwa/composables";
+import { useUser, useCart } from "@shopware-pwa/composables";
 import { ShippingMethod } from "@shopware-pwa/commons/interfaces/models/checkout/shipping/ShippingMethod";
 import { PaymentMethod } from "@shopware-pwa/commons/interfaces/models/checkout/payment/PaymentMethod";
 import { GuestOrderParams } from "@shopware-pwa/commons/interfaces/request/GuestOrderParams";
@@ -9,30 +9,32 @@ import {
   getAvailableShippingMethods,
   getAvailablePaymentMethods,
   createGuestOrder,
+  createOrder as createApiOrder,
 } from "@shopware-pwa/shopware-6-client";
-import { useCart } from "../hooks/useCart";
 
 /**
  * @alpha
  */
 export interface UseCheckout {
-  guestOrderParams: Ref<Readonly<Partial<GuestOrderParams | null>>>; // salutationId, name, surname, email - only for guest order
-  // shippingData: Ref<ShippingData | null>; // only for guest order
-  // billingData: Ref<BillingData | null>; // only for guest order
-  // shippingMethod: Ref<any>; // set by setShippingMethod and compute to get this - NOT implement by now
-  // paymentMethod: Ref<any>; // set by setPaymentMethod and compute to get this - NOT implement by now
   isGuestOrder: Readonly<Ref<boolean>>;
-  getShippingMethods: () => Promise<ShippingMethod[]>;
-  getPaymentMethods: () => Promise<PaymentMethod[]>;
-  // setShippingMethod: (shippingMethod: any) => Promise<Readonly<boolean>>;
-  // setPaymentMethod: (paymentMethod: any) => Promise<Readonly<boolean>>;
+  guestOrderParams: Ref<Readonly<Partial<GuestOrderParams | null>>>;
+  getShippingMethods: (options?: {
+    forceReload: boolean;
+  }) => Promise<Readonly<Ref<readonly ShippingMethod[]>>>;
+  getPaymentMethods: (options?: {
+    forceReload: boolean;
+  }) => Promise<Readonly<Ref<readonly PaymentMethod[]>>>;
   createOrder: () => Promise<Order>;
 }
 
 const orderData: {
   guestOrderParams: Partial<GuestOrderParams>;
+  shippingMethods: ShippingMethod[];
+  paymentMethods: PaymentMethod[];
 } = Vue.observable({
   guestOrderParams: {},
+  shippingMethods: [],
+  paymentMethods: [],
 });
 
 /**
@@ -40,50 +42,47 @@ const orderData: {
  */
 export const useCheckout = (): UseCheckout => {
   const { isLoggedIn } = useUser();
-  const { placeOrder, refreshCart } = useCart();
+  const { refreshCart } = useCart();
+  const shippingMethods: Readonly<Ref<readonly ShippingMethod[]>> = computed(
+    () => orderData.shippingMethods
+  );
+  const paymentMethods: Readonly<Ref<readonly PaymentMethod[]>> = computed(
+    () => orderData.paymentMethods
+  );
 
-  const getShippingMethods = async () => {
+  const getShippingMethods = async (
+    { forceReload } = { forceReload: false }
+  ) => {
+    if (shippingMethods.value.length && !forceReload) return shippingMethods;
     const shippingMethodsResponse = await getAvailableShippingMethods();
-    return shippingMethodsResponse.data || [];
+    orderData.shippingMethods = shippingMethodsResponse.data || [];
+    return shippingMethods;
   };
 
-  const getPaymentMethods = async () => {
+  const getPaymentMethods = async (
+    { forceReload } = { forceReload: false }
+  ) => {
+    if (paymentMethods.value.length && !forceReload) return paymentMethods;
     const paymentMethodsResponse = await getAvailablePaymentMethods();
-    return paymentMethodsResponse.data || [];
+    orderData.paymentMethods = paymentMethodsResponse.data || [];
+    return paymentMethods;
   };
 
   const createOrder = async () => {
-    // used from useCart; or move the logic here.
-    // important thing is to update context/cart under the hood and then just place an order using one shot :)
     try {
       if (isGuestOrder.value) {
-        console.error("CHECKOUT PLACE ORDER", orderData.guestOrderParams);
-        // console.error("CHECKOUT PLACE shipping", orderData.shippingData);
-        // console.error("CHECKOUT PLACE billling", orderData.billingData);
-
-        // const shippingAddress = {
-        //   ...(orderData?.shippingData || {}),
-        //   salutationId: orderData?.customerData?.salutationId,
-        // };
-        // const guestOrder = {
-        //   ...(orderData?.customerData || {}),
-        //   shippingAddress: shippingAddress,
-        //   billingAddress: orderData?.billingData?.differentThanShipping
-        //     ? orderData.billingData
-        //     : shippingAddress,
-        // };
-        try {
-          await createGuestOrder(
-            orderData.guestOrderParams as GuestOrderParams
-          );
-        } catch (e) {
-          console.error("ERROR PLACING GUEST ORDER", e);
-        }
+        return await createGuestOrder(
+          orderData.guestOrderParams as GuestOrderParams
+        );
       } else {
-        return placeOrder();
+        return await createApiOrder();
       }
     } catch (e) {
-      console.error("PROBLEM WITH PLACING ORDER", e);
+      console.error(
+        "[useCheckout][createOrder] isGuest:" + isGuestOrder.value,
+        e
+      );
+      throw e;
     } finally {
       await refreshCart();
     }
