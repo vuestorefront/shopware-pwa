8c1e875f00dbb9a3cf9f76f204f41ade62e50427
patzick <tomczyk.patryk@gmail.com>
2020-04-27 12:59:16 +0200

    chore: redirect

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,9 @@
-const getConfig = require("vuepress-bar");
-const barConfig = getConfig(`${__dirname}/..`);
-
 module.exports = {
   title: "Shopware-PWA",
   description: "Headless PWA for Shopware",
   themeConfig: {
-    sidebar: barConfig.sidebar,
     repo: "DivanteLtd/shopware-pwa",
     docsDir: "docs",
-    sidebarDepth: 2
-  }
+    sidebarDepth: 2,
+  },
 };
