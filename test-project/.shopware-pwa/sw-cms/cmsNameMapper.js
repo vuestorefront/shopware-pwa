3b22f1af321a25d382ed71508f51a02f5df06866
Marek Mularczyk <mmularczyk@divante.pl>
2020-05-05 10:38:08 +0200

    feat: creme-cycles demo

---
+++ test-project/.shopware-pwa/sw-cms/cmsNameMapper.js
@@ -0,0 +1,145 @@
+// const componentsMap = {"sw-cms/sections/CmsSectionDefault":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/sections/CmsSectionDefault","sw-cms/sections/CmsSectionSidebar":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/sections/CmsSectionSidebar","sw-cms/blocks/CmsBlockTextOnImage":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockTextOnImage","sw-cms/blocks/CmsBlockDefault":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockDefault","sw-cms/blocks/CmsBlockImageCover":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageCover","sw-cms/blocks/CmsBlockCategoryNavigation":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockCategoryNavigation","sw-cms/blocks/CmsBlockImageBubbleRow":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageBubbleRow","sw-cms/blocks/CmsBlockCenterText":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockCenterText","sw-cms/blocks/CmsBlockImageFourColumn":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageFourColumn","sw-cms/blocks/CmsBlockImageHighlightRow":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageHighlightRow","sw-cms/blocks/CmsBlockImageSimpleGrid":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageSimpleGrid","sw-cms/blocks/CmsBlockImageTextCover":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageTextCover","sw-cms/blocks/CmsBlockImageThreeColumn":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageThreeColumn","sw-cms/blocks/CmsBlockImageThreeCover":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageThreeCover","sw-cms/blocks/CmsBlockImageTwoColumn":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageTwoColumn","sw-cms/elements/CmsElementProductCard":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementProductCard","sw-cms/elements/CmsElementProductSlider":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementProductSlider","sw-cms/elements/CmsElementImage":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementImage","sw-cms/elements/CmsElementText":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementText","sw-cms/elements/CmsElementVideoVimeo":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementVideoVimeo","sw-cms/elements/CmsElementVideoYoutube":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementVideoYoutube","sw-cms/elements/CmsElementProductListing":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementProductListing","sw-cms/elements/CmsElementCategoryNavigation":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementCategoryNavigation","sw-cms/elements/CmsElementCategorySidebarFilter":"/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementCategorySidebarFilter"}
+const componentsMap = {
+  'sw-cms/sections/CmsSectionDefault': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/sections/CmsSectionDefault'
+    ),
+
+  'sw-cms/sections/CmsSectionSidebar': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/sections/CmsSectionSidebar'
+    ),
+
+  'sw-cms/blocks/CmsBlockTextOnImage': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockTextOnImage'
+    ),
+
+  'sw-cms/blocks/CmsBlockDefault': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockDefault'
+    ),
+
+  'sw-cms/blocks/CmsBlockImageCover': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageCover'
+    ),
+
+  'sw-cms/blocks/CmsBlockCategoryNavigation': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockCategoryNavigation'
+    ),
+
+  'sw-cms/blocks/CmsBlockImageBubbleRow': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageBubbleRow'
+    ),
+
+  'sw-cms/blocks/CmsBlockCenterText': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockCenterText'
+    ),
+
+  'sw-cms/blocks/CmsBlockImageFourColumn': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageFourColumn'
+    ),
+
+  'sw-cms/blocks/CmsBlockImageHighlightRow': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageHighlightRow'
+    ),
+
+  'sw-cms/blocks/CmsBlockImageSimpleGrid': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageSimpleGrid'
+    ),
+
+  'sw-cms/blocks/CmsBlockImageTextCover': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageTextCover'
+    ),
+
+  'sw-cms/blocks/CmsBlockImageThreeColumn': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageThreeColumn'
+    ),
+
+  'sw-cms/blocks/CmsBlockImageThreeCover': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageThreeCover'
+    ),
+
+  'sw-cms/blocks/CmsBlockImageTwoColumn': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/blocks/CmsBlockImageTwoColumn'
+    ),
+
+  'sw-cms/elements/CmsElementProductCard': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementProductCard'
+    ),
+
+  'sw-cms/elements/CmsElementProductSlider': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementProductSlider'
+    ),
+
+  'sw-cms/elements/CmsElementImage': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementImage'
+    ),
+
+  'sw-cms/elements/CmsElementText': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementText'
+    ),
+
+  'sw-cms/elements/CmsElementVideoVimeo': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementVideoVimeo'
+    ),
+
+  'sw-cms/elements/CmsElementVideoYoutube': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementVideoYoutube'
+    ),
+
+  'sw-cms/elements/CmsElementProductListing': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementProductListing'
+    ),
+
+  'sw-cms/elements/CmsElementCategoryNavigation': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementCategoryNavigation'
+    ),
+
+  'sw-cms/elements/CmsElementCategorySidebarFilter': () =>
+    import(
+      '/home/mmularczyk/Divante/Shopware-PWA DEMOS/test-project/node_modules/@shopware-pwa/default-theme/cms/elements/CmsElementCategorySidebarFilter'
+    ),
+}
+
+import cmsMap from 'sw-cms/cmsMap.json'
+
+function loadComponent(path) {
+  if (componentsMap[path]) return componentsMap[path]
+  return () => import(`sw-cms/CmsNoComponent`)
+}
+
+function resolveComponent(blockType, contentType) {
+  return loadComponent(`sw-cms/${blockType}/${cmsMap[blockType][contentType]}`)
+}
+
+export function getCmsSectionComponent(content) {
+  return resolveComponent('sections', content.type)
+}
+
+export function getCmsBlockComponent(content) {
+  return resolveComponent('blocks', content.type)
+}
+
+export function getCmsElementComponent(content) {
+  return resolveComponent('elements', content.type)
+}
---
+++ test-project/.shopware-pwa/sw-plugins/SwPluginTopNavigation.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!--  -->
+  </div>
+</template>
+<script>
+// 
+
+export default {
+  components: {
+    // 
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped></style>
