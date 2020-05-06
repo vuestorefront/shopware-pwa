a4365a1e4f12f10869e356126bc2ba5d25b18344
patzick <tomczyk.patryk@gmail.com>
2020-05-06 18:18:52 +0200

    feat: display thank you page with payment link if available

--- packages/default-theme/pages/success-page.vue
+++ packages/default-theme/pages/success-page.vue
@@ -1,37 +1,74 @@
 <template>
   <div class="success-page" :key="$route.fullPath">
-    <SfHeading level=2 title="Thank you" subtitle="for shopping with us!" class="success-page__heading" />
+    <a :href="paymentUrl" v-if="paymentUrl">
+      <SfButton>
+        Pay for your order
+      </SfButton>
+    </a>
+    <SfHeading
+      title="Thank you"
+      subtitle="for shopping with us!"
+      class="success-page__heading"
+    />
     <SfButton @click="$router.push('/')">
       <SfIcon icon="chevron_left" color="white" size="20px" />Return to homepage
     </SfButton>
   </div>
 </template>
 <script>
-import { SfButton, SfHeading, SfIcon } from "@storefront-ui/vue";
+import { SfButton, SfHeading, SfIcon } from '@storefront-ui/vue'
+import { getOrderPaymentUrl } from '@shopware-pwa/shopware-6-client'
+import {
+  ref,
+  getCurrentInstance,
+  onMounted,
+  computed,
+} from '@vue/composition-api'
 
 export default {
-  name: "SuccessPage",
-  data() {
-    return {
-    }
-  },
+  name: 'SuccessPage',
   components: {
     SfHeading,
     SfButton,
     SfIcon,
   },
-};
+  data() {
+    return {}
+  },
+  setup(params) {
+    const vm = getCurrentInstance()
+    const paymentUrl = ref(null)
+    const orderId = computed(() => vm.$route.query.orderId)
+    const fullPath = computed(() => vm.$route.fullPath)
+
+    onMounted(async () => {
+      try {
+        const resp = await getOrderPaymentUrl({
+          orderId: orderId.value,
+          finishUrl: window.location.origin + fullPath.value,
+        })
+        paymentUrl.value = resp.paymentUrl
+      } catch (e) {
+        console.error('Success page, payment link error', e)
+      }
+    })
+    return {
+      paymentUrl,
+    }
+  },
+}
 </script>
 <style lang="scss">
 @import '~@storefront-ui/vue/styles.scss';
 
 .success-page {
-  height: 100%;
-  text-align: center;
-  margin: auto;
+  min-height: 50vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
   &__heading {
-    height: 100%;
     margin-bottom: 30px;
     text-align: center;
     h2 {
