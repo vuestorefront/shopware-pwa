306956d2f4289c6eda4132f4c5555867280762fc
Michał Dziedziński <32803679+Michal-Dziedzinski@users.noreply.github.com>
2020-05-14 09:35:36 +0200

    fix(default-theme): mobile menu routes are fetched now (#735)

--- packages/default-theme/components/SwBottomNavigation.vue
+++ packages/default-theme/components/SwBottomNavigation.vue
@@ -67,6 +67,7 @@ import {
 } from '@shopware-pwa/composables'
 import { PAGE_ACCOUNT } from '../helpers/pages'
 import SwCurrency from '@shopware-pwa/default-theme/components/SwCurrency'
+import { onMounted } from '@vue/composition-api'
 
 export default {
   name: 'SwBottomNavigation',
@@ -86,9 +87,13 @@ export default {
   },
   setup() {
     const { toggleSidebar, isSidebarOpen } = useCartSidebar()
-    const { routes } = useNavigation()
+    const { routes, fetchRoutes } = useNavigation()
     const { toggleModal } = useUserLoginModal()
     const { isLoggedIn } = useUser()
+
+    onMounted(async () => {
+      await fetchRoutes()
+    })
     return {
       isLoggedIn,
       routes,
