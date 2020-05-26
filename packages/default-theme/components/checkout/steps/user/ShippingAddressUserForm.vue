4fdf1fdab1db47c5c118fe95e355b70c99fff73c
Przemysław Spaczek <kontakt@przemyslawspaczek.pl>
2020-05-26 16:31:02 +0200

    feat: shipping address user form

--- packages/default-theme/components/checkout/steps/user/ShippingAddressUserForm.vue
+++ packages/default-theme/components/checkout/steps/user/ShippingAddressUserForm.vue
@@ -1,24 +1,125 @@
 <template>
-  <div class="form">
-    <SfAlert
-      type="info"
-      message="TODO: implement logged user addresses management"
+  <div class="shipping-address-user-form">
+    <SfList class="shipping-address-user-form__list">
+      <SfListItem
+        v-for="address in addresses"
+        :key="address._uniqueIdentifier"
+        class="shipping-address-user-form__list-item"
+      >
+        <SfRadio
+          v-model="selectedAddress"
+          :value="address._uniqueIdentifier"
+          class="shipping-address-user-form__address address"
+        >
+          <template #checkmark>
+            <div class="sf-radio__checkmark address__checkmark" />
+          </template>
+          <template #label>
+            <p class="address__details">
+              {{ address.firstName }} {{ address.lastName }}<br />
+              {{ address.street }}<br />
+              {{ address.zipcode }}<br />
+              {{ address.city }}<br />
+              {{ address.country }}<br />
+              {{ address.phoneNumber }}
+            </p>
+          </template>
+        </SfRadio>
+      </SfListItem>
+    </SfList>
+    <SfCheckbox
+      v-model="useAsDefaultAddress"
+      label="Use this address as my default one"
+      class="shipping-address-user-form__default"
     />
+    <SfButton
+      class="sf-button color-secondary shipping-address-user-form__add-new"
+      >Add new</SfButton
+    >
   </div>
 </template>
 <script>
-import { SfAlert } from '@storefront-ui/vue'
-
+import { SfList, SfRadio, SfCheckbox, SfButton } from '@storefront-ui/vue'
+import { useUser } from '@shopware-pwa/composables'
 export default {
   name: 'ShippingAddressUserForm',
-  components: {
-    SfAlert,
-  },
+  components: { SfList, SfRadio, SfCheckbox, SfButton },
   setup() {
-    return {}
+    const { addresses, loadAddresses } = useUser()
+    loadAddresses()
+    return {
+      addresses,
+      loadAddresses,
+    }
+  },
+  data() {
+    return {
+      selectedAddress: '',
+      useAsDefaultAddress: false,
+    }
   },
 }
 </script>
 <style lang="scss" scoped>
 @import '@/assets/scss/variables';
+.shipping-address-user-form {
+  margin: 0 0 var(--spacer-xl) 0;
+  &__list {
+    margin: 0 0 var(--spacer-xl) 0;
+    @include for-desktop {
+      display: flex;
+    }
+  }
+  &__list-item {
+    margin: 0 0 var(--spacer-base) 0;
+    &:last-child {
+      margin: 0;
+    }
+    @include for-desktop {
+      flex: 0 0 50%;
+      margin: 0 var(--spacer-base) 0 0;
+      &:last-child {
+        margin: 0;
+      }
+    }
+  }
+  &__invoice {
+    margin: 0 0 var(--spacer-base) 0;
+  }
+  &__default {
+    margin: 0 0 var(--spacer-xl) 0;
+  }
+  &__add-new {
+    @include for-mobile {
+      --button-width: 100%;
+    }
+  }
+}
+.address {
+  --radio-container-padding: var(--spacer-sm) var(--spacer-base);
+  --radio-background: transparent;
+  --radio-checkmark-border-color: transparent;
+  --radio-content-margin: 0;
+  position: relative;
+  border: 1px solid var(--c-dark-variant);
+  transition: border-color 150ms ease-in-out;
+  @include for-desktop {
+    border-color: transparent;
+  }
+  &:hover {
+    border-color: var(--c-primary);
+  }
+  &__checkmark {
+    position: absolute;
+    top: var(--spacer-xs);
+    right: var(--spacer-xs);
+  }
+  &__details {
+    padding: 0;
+    margin: 0;
+  }
+  &.sf-radio--is-active {
+    border-color: var(--c-primary);
+  }
+}
 </style>
