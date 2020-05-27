3d5a8bf759f20f4287a7c09a83b0124fb64d775d
Maciej Kucmus <maciejkucmus@gmail.com>
2020-05-27 09:01:01 +0200

    fix: orders page

--- packages/default-theme/pages/_lang/account/orders.vue
+++ packages/default-theme/pages/_lang/account/orders.vue
@@ -4,10 +4,11 @@
 
 <script>
 export default {
-  name: 'OrderHistory';
+  name: 'OrderHistory'
 }
 </script>
 
 <style lang="scss" scoped>
 @import '@/assets/scss/variables';
-</script>
+
+</style>
