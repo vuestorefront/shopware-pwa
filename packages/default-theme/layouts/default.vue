b9ee74778062b64b28bc1cd95447ae3b678d552d
mkucmus <maciejkucmus@gmail.com>
2020-06-19 23:20:33 +0200

    fix(default-theme): add href to breadcrumb anchor

--- packages/default-theme/layouts/default.vue
+++ packages/default-theme/layouts/default.vue
@@ -72,6 +72,7 @@ export default {
     const getBreadcrumbs = computed(() =>
       Object.values(getBreadcrumbsObject.value).map((breadcrumb) => ({
         text: breadcrumb.name,
+        link: vm.$i18n.path(breadcrumb.path),
         route: {
           link: vm.$i18n.path(breadcrumb.path),
         },
