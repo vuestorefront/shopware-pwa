fac485158c36c7a269037f09bc52a6db953eec85
mkucmus <maciejkucmus@gmail.com>
2020-06-19 21:54:23 +0200

    fix: build the right storefront URL during registration

--- packages/default-theme/components/SwRegister.vue
+++ packages/default-theme/components/SwRegister.vue
@@ -178,7 +178,7 @@ export default {
         email: this.email,
         password: this.password,
         salutationId: this.salutation.id,
-        storefrontUrl: window?.location?.origin,
+        storefrontUrl: window && window.location && `${window.location.protocol}//${window.location.hostname}`,
         billingAddress: {
           firstName: this.firstName,
           salutationId: this.salutation.id,
