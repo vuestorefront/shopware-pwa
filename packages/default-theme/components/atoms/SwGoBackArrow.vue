924cd00634aa41f37b24340309de822c35009bfa
luklus <ll_code@outlook.com>
2020-06-23 15:12:24 +0200

    feat(default-theme): add go back button

---
+++ packages/default-theme/components/atoms/SwGoBackArrow.vue
@@ -0,0 +1,34 @@
+<template>
+  <SfArrow aria-label="Go back arrow" @click="goBack" class="go-back" />
+</template>
+<script>
+import { SfArrow } from "@storefront-ui/vue"
+
+export default {
+  name: "SwGoBackArrow",
+
+  components: {
+    SfArrow,
+  },
+
+  setup(props, { root }) {
+    return {}
+  },
+
+  methods: {
+    goBack() {
+      this.$router.back()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables";
+
+.go-back {
+  @include for-desktop {
+    display: none;
+  }
+}
+</style>
