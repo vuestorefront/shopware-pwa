aeda721f3c1fdb2a9778a418c88b2f6592aac98c
Przemyslaw Koroscik <przemyslaw.koroscik@gmail.com>
2020-07-08 13:50:18 +0200

    fix(default-theme): fixed more categories

--- packages/default-theme/components/SwTopNavigation.vue
+++ packages/default-theme/components/SwTopNavigation.vue
@@ -136,14 +136,22 @@ export default {
         const nav = this.$refs.navigation
         const navElements = nav.querySelectorAll(".sf-header-navigation-item")
         let visibleItemsCount = 0
+        let unvisibleItemsCount = 0
 
         navElements.forEach((element) => {
           if (element.offsetTop === 0) {
             visibleItemsCount += 1
           }
+          if (element.offsetTop > 0) {
+            unvisibleItemsCount += 1
+          }
         })
-        // This subtraction makes more space in nav and prevent to move "more category " to the next line.
-        this.unwrappedElements = Math.max(0, visibleItemsCount - 2)
+        if (unvisibleItemsCount >= 1) {
+          // This subtraction makes more space in nav and prevent to move "more category " to the next line.
+          this.unwrappedElements = Math.max(0, visibleItemsCount - 2)
+        } else {
+          this.unwrappedElements = visibleItemsCount
+        }
       })
     },
   },
