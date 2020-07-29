8b862590564917a572159201f5f077c462e7683d
Patryk Tomczyk <13100280+patzick@users.noreply.github.com>
2020-07-29 13:23:33 +0200

    fix: windows problem with build

---
+++ packages/cli/src/stages.ts
@@ -0,0 +1,5 @@
+export const STAGES = {
+  STABLE: "latest stable (recommended)",
+  CANARY: "canary (current master branch)",
+  LOCAL: "local contibution (to contribute locally in shopware-pwa)",
+};
