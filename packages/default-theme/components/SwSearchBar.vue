68a58b701f896486c2d0db3f33e375c8c7da7d1d
patzick <tomczyk.patryk@gmail.com>
2020-07-30 15:23:06 +0200

    chore: move debounce to helper and style menu visibility

--- packages/default-theme/components/SwSearchBar.vue
+++ packages/default-theme/components/SwSearchBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="sw-search-bar">
     <SfSearchBar
       :placeholder="$t('Search for products')"
       :aria-label="$t('Search for products')"
@@ -25,6 +25,7 @@ import { ref, reactive, onMounted, watch, computed } from "@vue/composition-api"
 import { getSearchPageUrl } from "@shopware-pwa/default-theme/helpers"
 import { SfSearchBar } from "@storefront-ui/vue"
 import { useProductSearch } from "@shopware-pwa/composables"
+import { debounce } from "@shopware-pwa/helpers"
 import SwSuggestSearch from "@shopware-pwa/default-theme/components/SwSuggestSearch"
 
 export default {
@@ -52,6 +53,19 @@ export default {
 
     const isSuggestSearchVisible = computed(() => isSuggestBoxOpen.value)
 
+    const performSuggestSearch = debounce((event) => {
+      if (event && event.key === "Enter") {
+        return
+      }
+      const searchTerm = event.target.value
+      if (typeof searchTerm === "string" && searchTerm.length > 0) {
+        suggestSearch(searchTerm)
+        isSuggestBoxOpen.value = true
+      } else {
+        isSuggestBoxOpen.value = false
+      }
+    }, 300)
+
     return {
       currentSearchTerm,
       search,
@@ -63,36 +77,10 @@ export default {
       getSearchPageUrl,
       typingQuery,
       resetFilters,
-      debounce,
-    }
-  },
-  data() {
-    return {
-      debounce: null,
+      performSuggestSearch,
     }
   },
   methods: {
-    performSuggestSearch(event) {
-      clearTimeout(this.debounce)
-
-      if (event && event.key === "Enter") {
-        return
-      }
-
-      this.debounce = setTimeout(() => {
-        const searchTerm = event.target.value
-        if (typeof searchTerm === "string" && searchTerm.length > 0) {
-          try {
-            this.suggestSearch(searchTerm)
-            this.isSuggestBoxOpen = true
-          } catch (e) {
-            console.error("[SwTopNavigation][performSuggestSearch]: " + e)
-          }
-        } else {
-          this.isSuggestBoxOpen = false
-        }
-      }, 400)
-    },
     performSearch(searchTerm) {
       if (typeof searchTerm === "string" && searchTerm.length > 0) {
         this.resetFilters()
@@ -114,36 +102,11 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/scss/variables";
 
-.sw-top-navigation {
+::v-deep .sf-search-bar {
   --search-bar-width: 100%;
-  --header-container-padding: 0 var(--spacer-base);
-  --header-navigation-item-margin: 0 1rem 0 0;
-  margin-bottom: var(--spacer-sm);
-  .sw-overlay {
-    --overlay-z-index: 1;
-  }
-
-  @include for-desktop {
-    ::v-deep .sf-header {
-      display: flex;
-      justify-content: space-between;
-      &__sticky-container {
-        width: 100%;
-      }
-      &__navigation {
-        flex: 0 0 calc(100% - 20rem);
-      }
-    }
-  }
 }
-.sw-header {
-  z-index: 2;
-  background-color: #fff;
-  &__icons {
-    display: flex;
-  }
-  &__icon {
-    cursor: pointer;
-  }
+
+.sw-search-bar {
+  width: 100%;
 }
 </style>
