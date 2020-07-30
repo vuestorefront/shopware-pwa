c03e11dcd789fb97c8960f260beb30fa17bde854
mkucmus <maciejkucmus@gmail.com>
2020-07-30 18:07:05 +0200

    fix(composables): change the includes configuration for page resolver

--- packages/composables/src/internalHelpers/includesParameter.ts
+++ packages/composables/src/internalHelpers/includesParameter.ts
@@ -17,46 +17,47 @@ const PRODUCT_GROUP = ["id", "name", "options", "translated"];
 const PRODUCT_GROUP_OPTION = ["name", "id", "group", "translated"];
 const PRODUCT_CALCULATED_PRICE = ["unitPrice", "quantity"];
 const MEDIA = ["url"];
-const CMS_PAGE = ["id", "name", "sections", "type", "config"];
-const CMS_PAGE_SECTION = [
-  "id",
-  "backgroundMedia",
-  "blocks",
-  "type",
-  "sizingMode",
-];
-const CMS_PAGE_BLOCK = [
-  "slots",
-  "type",
-  "id",
-  "backgroundMedia",
-  "sectionPosition",
-];
-const CMS_PAGE_SLOT = [
-  "id",
-  "type",
-  "slot",
-  "blockId",
-  "data",
-  "backgroundMediaMode",
-  "backgroundMedia",
-];
+// const CMS_PAGE = ["id", "name", "sections", "type", "config"];
+// const CMS_PAGE_SECTION = [
+//   "id",
+//   "backgroundMedia",
+//   "blocks",
+//   "type",
+//   "sizingMode",
+// ];
+// const CMS_PAGE_BLOCK = [
+//   "slots",
+//   "type",
+//   "id",
+//   "backgroundMedia",
+//   "sectionPosition",
+// ];
+// const CMS_PAGE_SLOT = [
+//   "id",
+//   "type",
+//   "slot",
+//   "blockId",
+//   "data",
+//   "backgroundMediaMode",
+//   "backgroundMedia",
+// ];
 
 /**
  * Parameters for page resolver - aligned with getPage method of @shopware-pwa/shopware-6-client
  */
-export const getPageIncludes = () => ({
-  cms_page_slot: CMS_PAGE_SLOT,
-  cms_page_block: CMS_PAGE_BLOCK,
-  cms_page_section: CMS_PAGE_SECTION,
-  cms_page: CMS_PAGE,
-  product: PRODUCT,
-  product_media: PRODUCT_MEDIA,
-  media: MEDIA,
-  calculated_price: PRODUCT_CALCULATED_PRICE,
-  product_group_option: PRODUCT_GROUP_OPTION,
-  product_group: PRODUCT_GROUP,
-});
+export const getPageIncludes = () => ({});
+// ({
+//   cms_page_slot: CMS_PAGE_SLOT,
+//   cms_page_block: CMS_PAGE_BLOCK,
+//   cms_page_section: CMS_PAGE_SECTION,
+//   cms_page: CMS_PAGE,
+//   product: PRODUCT,
+//   product_media: PRODUCT_MEDIA,
+//   media: MEDIA,
+//   calculated_price: PRODUCT_CALCULATED_PRICE,
+//   product_group_option: PRODUCT_GROUP_OPTION,
+//   product_group: PRODUCT_GROUP,
+// });
 
 /**
  * Parameters for product listing - aligned with getProductListing method of @shopware-pwa/shopware-6-client
