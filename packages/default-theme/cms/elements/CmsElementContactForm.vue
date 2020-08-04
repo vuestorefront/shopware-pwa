acae33f5ce97f0033ab4b0f7a3dbe01d9f560123
patzick <tomczyk.patryk@gmail.com>
2020-08-04 11:53:37 +0200

    chore: fix contact form flow

--- packages/default-theme/cms/elements/CmsElementContactForm.vue
+++ packages/default-theme/cms/elements/CmsElementContactForm.vue
@@ -23,9 +23,9 @@
         <SfInput
           v-model="fname"
           :type="formFields.textType"
-          :label="`First name`"
-          :name="`firstname`"
-          :error-message="`First name is required`"
+          label="First name"
+          name="firstname"
+          error-message="First name is required"
           :disabled="false"
           :valid="!$v.fname.$error"
           class="small input"
@@ -35,9 +35,9 @@
         <SfInput
           v-model="lname"
           :type="formFields.textType"
-          :label="`Last name`"
-          :name="`lastname`"
-          :error-message="`Last name is required`"
+          label="Last name"
+          name="lastname"
+          error-message="Last name is required"
           :disabled="false"
           :valid="!$v.lname.$error"
           class="small input"
@@ -49,71 +49,83 @@
         <SfInput
           v-model="email"
           :type="formFields.textType"
-          :label="`Email address`"
-          :name="`email`"
-          :error-message="`Email is required`"
+          label="Email address"
+          name="email"
+          error-message="Email is required"
           :disabled="false"
           :valid="!$v.email.$error"
           class="small input"
           @blur="$v.email.$touch()"
         />
-
         <SfInput
           v-model="phone"
           :type="formFields.textType"
-          :label="`Phone number`"
-          :name="`phone`"
-          :error-message="`Enter valid phone number`"
+          label="Phone number"
+          name="phone"
+          error-message="Enter valid phone number"
           :disabled="false"
           :valid="!$v.phone.$error"
           @blur="$v.phone.$touch()"
         />
       </div>
 
-      <SfInput
-        v-model="subject"
-        :type="formFields.textType"
-        :label="`Subject line`"
-        :name="`subject`"
-        :error-message="`Subject is required`"
-        :disabled="false"
-        :valid="!$v.subject.$error"
-        @blur="$v.subject.$touch()"
+      <div class="input-group">
+        <SfInput
+          v-model="subject"
+          :type="formFields.textType"
+          label="Subject line"
+          name="subject"
+          error-message="Subject is required"
+          :disabled="false"
+          :valid="!$v.subject.$error"
+          @blur="$v.subject.$touch()"
+        />
+      </div>
+      <div class="input-group">
+        <SfInput
+          v-model="message"
+          :type="formFields.textType"
+          label="Your message"
+          name="message"
+          error-message="Message is required"
+          :disabled="false"
+          :valid="!$v.message.$error"
+          @blur="$v.message.$touch()"
+        />
+      </div>
+
+      <div class="input-group">
+        <SfCheckbox
+          v-model="checkbox"
+          name="checkbox"
+          :label="getConfirmationText"
+          :valid="!$v.checkbox.$error"
+          error-message="Required"
+          @blur="$v.checkbox.$touch()"
+        />
+      </div>
+
+      <SfAlert
+        v-if="$v.$invalid && errorMessage"
+        :message="errorMessage"
+        type="danger"
       />
 
-      <SfInput
-        v-model="message"
-        :type="formFields.textType"
-        :label="`Your message`"
-        :name="`message`"
-        :error-message="`Message is required`"
-        :disabled="false"
-        :valid="!$v.message.$error"
-        @blur="$v.message.$touch()"
-      />
-
-      <SfCheckbox
-        v-model="checkbox"
-        :name="`checkbox`"
-        :label="`I have read the data protection information.`"
-        :valid="!$v.checkbox.$error"
-        @blur="$v.checkbox.$touch()"
-      />
-
-      <SfButton class="send button">
-        send
-      </SfButton>
+      <SwButton class="send button">
+        {{ $t("send") }}
+      </SwButton>
     </form>
   </div>
 </template>
 
 <script>
-import { SfSelect, SfInput, SfCheckbox, SfButton } from "@storefront-ui/vue"
+import { SfSelect, SfInput, SfCheckbox, SfAlert } from "@storefront-ui/vue"
 import { validationMixin } from "vuelidate"
 import { required, email, minLength } from "vuelidate/lib/validators"
 import { mapSalutations } from "@shopware-pwa/helpers"
 import { useSalutations } from "@shopware-pwa/composables"
 import { computed } from "@vue/composition-api"
+import SwButton from "@shopware-pwa/default-theme/components/atoms/SwButton"
 
 export default {
   name: "CmsElementContactForm",
@@ -121,7 +133,8 @@ export default {
     SfSelect,
     SfInput,
     SfCheckbox,
-    SfButton,
+    SwButton,
+    SfAlert,
   },
   mixins: [validationMixin],
   props: {
@@ -146,8 +159,8 @@ export default {
       phone: "",
       subject: "",
       message: "",
-      checkbox: "",
-      checked: false,
+      checkbox: false,
+      errorMessage: "",
     }
   },
   setup(props, { root }) {
@@ -156,32 +169,30 @@ export default {
       mapSalutations(getSalutations.value)
     )
 
+    const getConfirmationText = computed(
+      () =>
+        props?.content?.config?.confirmationText?.value ||
+        "I have read and agree with the data protection regulations."
+    )
+
     return {
       getMappedSalutations,
       salutationsError,
+      getConfirmationText,
     }
   },
-  computed: {
-    getSlots() {
-      return this.content.slots || []
-    },
-    getContent() {
-      return this.getSlots.length && this.getSlots[0]
-    },
-  },
   methods: {
-    submit() {
+    async submit() {
+      this.errorMessage = ""
       this.$v.$touch()
       if (this.$v.$invalid) {
-        this.submitStatus = "ERROR"
-        console.log("error")
-      } else {
-        this.submitStatus = "PENDING"
-        setTimeout(() => {
-          this.submitStatus = "OK"
-        }, 500)
-        console.log("sending")
+        this.errorMessage = this.$t(
+          "Please fill form data and check regulations acceptance."
+        )
+        return
       }
+
+      console.error("SENDING")
     },
   },
   validations: {
@@ -213,6 +224,7 @@ export default {
     },
     checkbox: {
       required,
+      isTrue: (value) => value === true,
     },
   },
 }
@@ -226,10 +238,10 @@ export default {
   .input {
     &-group {
       display: flex;
+      margin-bottom: var(--spacer-xl);
 
       > * {
         margin-right: var(--spacer-sm);
-        margin-bottom: var(--spacer-sm);
         width: 100%;
       }
     }
