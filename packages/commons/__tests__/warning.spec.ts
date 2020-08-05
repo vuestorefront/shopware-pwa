5aeb651e3161d0f4e670b375dd0153288ccab581
mkucmus <maciejkucmus@gmail.com>
2020-08-05 17:50:35 +0200

    feat(client): handle multiple sorting (#988)

---
+++ packages/commons/__tests__/warning.spec.ts
@@ -0,0 +1,42 @@
+import { warning } from "@shopware-pwa/commons";
+describe("helpers", () => {
+  const originalWarn = console.warn;
+  afterEach(() => (console.warn = originalWarn));
+  const OLD_ENV = process.env;
+
+  describe("warning", () => {
+    beforeEach(() => {
+      process.env = { ...OLD_ENV };
+    });
+    afterAll(() => {
+      process.env = OLD_ENV;
+    });
+
+    it("should invoke console.warn in NODE_ENV other than production", () => {
+      const mockedWarn = jest.fn();
+      console.warn = mockedWarn;
+      process.env.NODE_ENV = "dev";
+      warning({
+        packageName: "helpers",
+        methodName: "warning",
+        notes: "some warning",
+      });
+
+      expect(mockedWarn).toBeCalledWith(
+        "[PROBLEM][@shopware-pwa/helpers][warning]: some warning"
+      );
+    });
+    it("should invoke console.warn in NODE_ENV other than production", () => {
+      const mockedWarn = jest.fn();
+      console.warn = mockedWarn;
+      process.env.NODE_ENV = "production";
+      warning({
+        packageName: "helpers",
+        methodName: "warning",
+        notes: "some warning",
+      });
+
+      expect(mockedWarn).not.toBeCalled();
+    });
+  });
+});
