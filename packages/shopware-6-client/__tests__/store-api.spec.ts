e1202bd480860e451f4ff2bbbf53a9a6351beedd
mkucmus <maciejkucmus@gmail.com>
2020-08-11 10:35:13 +0200

    test(client): test endpoints coverage

---
+++ packages/shopware-6-client/__tests__/store-api.spec.ts
@@ -0,0 +1,28 @@
+import { invokeGet } from "@shopware-pwa/shopware-6-client";
+import * as allEndpoints from "../src/endpoints";
+
+describe("store-api", () => {
+  const endpoints = Object.keys(allEndpoints);
+  const ApiEndpointsMap = endpoints.map((functionName: string) =>
+    // @ts-ignore
+    allEndpoints[functionName]()
+  );
+  it("should contain all the methods implemented", async () => {
+    const {
+      data: {
+        paths,
+        info: { version },
+      },
+    } = await invokeGet({ address: `/store-api/v3/_info/openapi3.json` });
+    const regex = /(\{[\w-A-Za-z]+})/g;
+
+    const coveredEndpoints = Object.keys(paths)
+      .filter((path) => path.includes("store-api"))
+      .map((path) => {
+        const endpointPath = path.replace(regex, "undefined");
+        const endpoint = `/store-api/v${version}${endpointPath}`;
+        return endpoint;
+      });
+    expect(ApiEndpointsMap).toEqual(expect.arrayContaining(coveredEndpoints));
+  });
+});
