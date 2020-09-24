85e59cf25ef1b7074445afc4b57767fa92054c1f
mkucmus <maciejkucmus@gmail.com>
2020-09-24 23:24:30 +0200

    feat(helpers): use pretty URLs for product page

--- packages/helpers/__tests__/product/getProductUrl.spec.ts
+++ packages/helpers/__tests__/product/getProductUrl.spec.ts
@@ -1,13 +1,28 @@
 import { getProductUrl } from "@shopware-pwa/helpers";
 
 describe("Shopware helpers - getProductUrl", () => {
-  it("should return product url", () => {
+  it("should return product technical URL", () => {
     const result = getProductUrl({ id: "qwerty" } as any);
+    expect(result).toEqual("/detail/qwerty");
+  });
+
+  it("should return default URL if seoUrls is undefined", () => {
+    const result = getProductUrl({ id: "qwerty", seoUrls: undefined } as any);
     expect(result).toEqual("/detail/qwerty");
   });
 
   it("should return default url for no product", () => {
     const result = getProductUrl(undefined as any);
     expect(result).toEqual("/");
+  });
+  it("should return seo URL if available", () => {
+    const result = getProductUrl({
+      seoUrls: [
+        {
+          seoPathInfo: "pretty-url/012345",
+        },
+      ],
+    } as any);
+    expect(result).toEqual("/pretty-url/012345");
   });
 });
