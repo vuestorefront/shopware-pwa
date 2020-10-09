aad8b5ba36b00878b538d69404d49bf981ee3564
patzick <13100280+patzick@users.noreply.github.com>
2020-10-09 13:19:01 +0200

    chore: update after moving from setup

--- packages/default-theme/components/SwSearchBar.vue
+++ packages/default-theme/components/SwSearchBar.vue
@@ -73,7 +73,7 @@ export default {
     },
     typingQuery(value) {
       if (!this.isMobile) {
-        performSuggestSearch(typingQuery.value)
+        this.performSuggestSearch(value)
       }
     },
   },
