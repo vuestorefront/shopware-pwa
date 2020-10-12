824a3dfa0c3fac6bfe612d5807e5e4ca72bebdd8
patzick <13100280+patzick@users.noreply.github.com>
2020-10-12 11:30:50 +0200

    feat: build theme base package

---
+++ packages/theme-base/src/pages/_lang/_.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>You can show your CMS page overriding "src/pages/_lang/_.vue" file.</h2>
+</template>
+<script>
+export default {
+  name: "DynamicRoute",
+  components: {},
+  watchQuery(newQuery, oldQuery) {
+    // Only execute component methods if currency changed
+    return newQuery.currencyId !== oldQuery.currencyId;
+  },
+  asyncData: async ({ params, app, error: errorView, query }) => {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped></style>
