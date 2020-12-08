2bb3575281f1a42e04a237bb34ce1f5ac65c4318
mkucmus <maciejkucmus@gmail.com>
2020-12-08 10:17:24 +0100

    fix(helpers): get name fallback if there is no translation (#1279)

--- packages/helpers/src/navigation/getStoreNavigationRoutes.ts
+++ packages/helpers/src/navigation/getStoreNavigationRoutes.ts
@@ -17,7 +17,7 @@ export function getStoreNavigationRoutes(
   navigationElements: StoreNavigationElement[]
 ): StoreNavigationRoute[] {
   return navigationElements.map((element: StoreNavigationElement) => ({
-    routeLabel: element.translated.name,
+    routeLabel: element.translated?.name || element.name,
     isExternal: !!element.externalLink,
     routePath:
       element.externalLink ||
