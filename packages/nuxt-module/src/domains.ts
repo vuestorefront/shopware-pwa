f956fa4264e040ff36e0b06fcc716ea826561691
mkucmus <maciejkucmus@gmail.com>
2021-01-21 22:54:06 +0100

    fix: routes order

--- packages/nuxt-module/src/domains.ts
+++ packages/nuxt-module/src/domains.ts
@@ -56,7 +56,13 @@ export async function setupDomains(
   const enrichRoutes = (routes: NuxtRouteConfig[]) => {
     // reverse routes to set the global wildcard /* as a last one in the matching table
     // to match always the custom ones first, the /* should be a path of the last chance (page resolver)
-    routes.reverse().forEach((route) => {
+    // flip an array if the first element is wildard for all other routes /*
+    let routesOrdered = routes;
+    if (routes[0].name.startsWith("all")) {
+      routesOrdered = routes.reverse();
+    }
+
+    routesOrdered.forEach((route) => {
       // skip route enrichment if there is domainId attached with other configuration
       if (!route.meta?.domainId || !domainsEntries.length) {
         domainsEntries.forEach((domainConfig) => {
@@ -85,6 +91,10 @@ export async function setupDomains(
       }
     });
     if (domainsRoutes.length) {
+      const wildCardIndexFound = domainsRoutes.findIndex((v) => v.path == "/*");
+      // force the path of last change to be last one in the routes list
+      wildCardIndexFound &&
+        domainsRoutes.push(domainsRoutes.splice(wildCardIndexFound, 1)[0]);
       routes.splice(0, routes.length, ...domainsRoutes); // force replace the new routes table
     }
   };
