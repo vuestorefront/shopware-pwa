d264f1c340721d09578f67e11e92be3442c9b28f
mkucmus <maciejkucmus@gmail.com>
2021-02-02 22:18:04 +0100

    fix(nuxt-module): proper i18n plugin injection

--- packages/default-theme/src/logic/notifications/index.js
+++ packages/default-theme/src/logic/notifications/index.js
@@ -3,7 +3,7 @@ import { useNotifications } from "@shopware-pwa/composables"
 export const addToCartNotification = (product, rootContext) => {
   const { pushSuccess } = useNotifications(rootContext)
   pushSuccess(
-    rootContext.i18n.t("{productName} has been added to cart.", {
+    rootContext.$t("{productName} has been added to cart.", {
       productName: product?.translated?.name || product?.name,
     })
   )
@@ -16,10 +16,10 @@ export const addPromotionCodeNotification = (result, rootContext) => {
   if (err) {
     switch (err.messageKey) {
       case "promotion-discount-added":
-        pushSuccess(rootContext.i18n.t("Promotion code added successfully"))
+        pushSuccess(rootContext.$t("Promotion code added successfully"))
         break
       case "promotion-not-found":
-        pushError(rootContext.i18n.t("Promotion code does not exist"))
+        pushError(rootContext.$t("Promotion code does not exist"))
         break
       default:
         pushError(err.message.toString())
