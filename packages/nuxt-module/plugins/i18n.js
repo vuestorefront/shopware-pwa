8732d386cfa73eac80380b87c0256ec769f5baee
mkucmus <maciejkucmus@gmail.com>
2021-02-08 11:30:27 +0100

    fix(nuxt-module): proper i18n plugin injection (#1334)
    * fix(default-theme): get the product payload correctly

--- packages/nuxt-module/plugins/i18n.js
+++ packages/nuxt-module/plugins/i18n.js
@@ -3,10 +3,10 @@ import VueI18n from "vue-i18n";
 
 Vue.use(VueI18n);
 
-export default ({ app, store }) => {
+export default ({ app, store }, inject) => {
   // Set i18n instance on app
   // This way we can use it in middleware and pages asyncData/fetch
-  app.i18n = new VueI18n({
+  const i18n = new VueI18n({
     locale: store.state.locale,
     fallbackLocale: "<%= options.defaultLanguage %>",
     messages: {
@@ -15,4 +15,7 @@ export default ({ app, store }) => {
       // <% }) %>
     },
   });
+
+  app.i18n = i18n;
+  inject("i18n", i18n);
 };
