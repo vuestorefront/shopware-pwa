20c7672f2050951b3a1283d3b93ea2db5f9f91ba
mkucmus <maciejkucmus@gmail.com>
2021-08-05 22:58:20 +0200

    fix(default-theme): prevent from calling page resolver on static pages

--- packages/default-theme/src/middleware/pages.js
+++ packages/default-theme/src/middleware/pages.js
@@ -1,5 +1,5 @@
 import { useBreadcrumbs, useCms } from "@shopware-pwa/composables"
-
+import { isStaticPage } from "@/helpers/pages"
 /**
  * A place to plug in some actions during changing the pages/routes.
  * For instance: broadcast events for specific page
@@ -10,10 +10,11 @@ export default async function ({ app, route, redirect }) {
   clear()
 
   const { search, currentSearchPathKey, page } = useCms(app)
-  if (route.path !== currentSearchPathKey.value) {
-    await search(route.path, route.query)
+  if (!isStaticPage(route) && route.path !== currentSearchPathKey.value) {
+    await search(route.params.pathMatch, route.query)
     // redirect to the cannnical URL if current path does not match the canonical one
-    if (page.value && route.path !== page.value.canonicalPathInfo) {
+    // route path shouldn't have virtual domain's prefix
+    if (page.value && route.params.pathMatch !== page.value.canonicalPathInfo) {
       return redirect(app.$routing.getUrl(page.value.canonicalPathInfo))
     }
   }
