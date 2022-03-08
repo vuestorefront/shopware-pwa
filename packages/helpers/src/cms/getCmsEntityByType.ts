e8655fb0c75b7fb2f1f2fc3dc80e0e6ec95a9706
mkucmus <maciejkucmus@gmail.com>
2022-03-08 13:47:20 +0100

    feat(composables): handle meta title and description in useCms composable

---
+++ packages/helpers/src/cms/getCmsEntityByType.ts
@@ -0,0 +1,21 @@
+import {
+  CmsPageResponse,
+  Product,
+  Category,
+} from "@shopware-pwa/commons";
+
+/**
+ * @public
+ */
+export function getCmsEntityByType(page?: CmsPageResponse | null): Product | Category | undefined {
+  if (!page?.resourceType) {
+    return;
+  }
+
+  switch (page.resourceType) {
+    case "frontend.navigation.page":
+      return page.category;
+    case "frontend.detail.page":
+      return page.product;
+  }
+}
