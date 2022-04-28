158b55a0c0a0021a5206eb38dc8fd03b2dc181d3
Maciek Kucmus <maciejkucmus@gmail.com>
2022-04-28 13:39:12 +0200

    fix(client): reveal all errors coming from API within HTTP 4xx group (#1866)
    * fix(client): uncover all errors related with HTTP 4xx status group
    
    * chore: tests
    
    Co-authored-by: Patryk Tomczyk <13100280+patzick@users.noreply.github.com>

--- packages/shopware-6-client/src/interceptors/errorInterceptor.ts
+++ packages/shopware-6-client/src/interceptors/errorInterceptor.ts
@@ -6,10 +6,18 @@ import {
 } from "@shopware-pwa/commons";
 
 /**
- * http status codes thrown by API
+ * Handle all HTTP status codes from 4xx group as an API errors, including 500 (exception for order placement issue)
+ * 408 timeout error is handled separately
  */
-const API_ERROR_CODES = [400, 401, 403, 404, 409, 410, 412, 424, 500];
-
+const isApiError = (statusCode: number): boolean => {
+  if (
+    (statusCode != 408 && statusCode.toString().startsWith("4")) ||
+    statusCode == 500
+  ) {
+    return true;
+  }
+  return false;
+};
 /**
  * @param {ShopwareApiError} error
  */
@@ -20,6 +28,10 @@ const extractApiErrorStatusCode = (error: ShopwareApiError): number => {
   );
 };
 
+/**
+ * Sometimes the HTTP status code is not available and must be guessed from the message.
+ * In cases like connection problems, or timeout error comes from intermediate layer (i.e. client)
+ */
 const guessTheStatusCodeFromTheMessage = (message: string): number => {
   // catch the specific timeout rejection from axios
   if (typeof message === "string" && message.startsWith("timeout of")) {
@@ -78,7 +90,7 @@ export async function errorInterceptor(
   // Do something with response error
   const statusCode = extractApiErrorStatusCode(error);
   const clientApiError: ClientApiError = {
-    messages: API_ERROR_CODES.includes(statusCode)
+    messages: isApiError(statusCode)
       ? extractApiErrorMessage(error)
       : extractNotApiErrorMessage(error),
     statusCode: statusCode,
