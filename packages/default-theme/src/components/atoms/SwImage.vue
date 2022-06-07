7d02980e8ba1a65c1696160cb91da937369d57b2
DANLAP066 <danlap066@VNDANLAP-0066.local>
2022-06-07 10:30:43 +0700

    feat(default-theme): Improve SwImage component with srcset

--- packages/default-theme/src/components/atoms/SwImage.vue
+++ packages/default-theme/src/components/atoms/SwImage.vue
@@ -1,21 +1,41 @@
 <template>
-  <SfImage v-bind="{ ...$props, ...$attrs }" :placeholder="placeholder" />
+  <SfImage :src="imgUrl" :srcsets="srcsets" v-bind="{ ...$props, ...$attrs }" :placeholder="placeholder" />
 </template>
 
 <script>
 import { SfImage } from "@storefront-ui/vue"
 import getPlaceholderImage from "@/helpers/images/getPlaceholderImage.js"
+import { computed } from "@vue/composition-api"
 
 export default {
   name: "SwImage",
   components: {
     SfImage,
   },
+  props: {
+    srcset: Array,
+    src: {
+      type: String,
+      default: ''
+    }
+  },
   setup(props, { root }) {
     const width = (props.imageWidth && props.imageWidth + "px") || "100%"
     const height = (props.imageHeight && props.imageHeight + "px") || "100%"
+    const srcsets = computed(() => props.srcset?.map(item => ({
+      src: item.url,
+      width: item.width,
+      resolution: item.width
+      })) ?? []
+    )
+    const imgUrl = computed(() => props.srcset?.reduce(function (res, thumb) {
+      return thumb.width < res.width ? thumb : res;
+    })?.url ?? props.src)
+
     return {
       placeholder: getPlaceholderImage(width, height),
+      srcsets,
+      imgUrl
     }
   },
 }
