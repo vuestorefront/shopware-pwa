21929e15fd3ad58b76cd73c52dac98221863c88b
Rittou <85224631+rittou@users.noreply.github.com>
2022-06-15 17:06:32 +0700

    fix(default-theme): Adding top navigation container for using out of header (#1898)

---
+++ packages/default-theme/src/components/SwTopNavigationContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="nav-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  .nav-container {
+    z-index: 1;
+    position: relative;
+  }
+</style>
