f3f538ac6b32a00f633ff3bf70143bcddd83a943
mkucmus <maciejkucmus@gmail.com>
2022-06-24 15:41:24 +0200

    feat(client): method to fetch seo urls

--- packages/shopware-6-client/src/services/pageService.ts
+++ packages/shopware-6-client/src/services/pageService.ts
@@ -1,6 +1,12 @@
 import { getPageResolverEndpoint, getSeoUrlEndpoint } from "../endpoints";
 import { defaultInstance, ShopwareApiInstance } from "../apiService";
-import { ShopwareSearchParams, CmsPageResponse } from "@shopware-pwa/commons";
+import {
+  ShopwareSearchParams,
+  CmsPageResponse,
+  EntityResult,
+  SeoUrl,
+} from "@shopware-pwa/commons";
+import { invokePost } from "./pluginService";
 
 /**
  * @throws ClientApiError
@@ -52,4 +58,25 @@ export async function getSeoUrls(
   });
 
   return resp.data;
+}
+
+/**
+ *
+ *
+ * @public
+ * @throws ClientApiError
+ */
+export async function getSeoUrl(
+  params: ShopwareSearchParams,
+  contextInstance: ShopwareApiInstance = defaultInstance
+): Promise<EntityResult<"seo_url", SeoUrl[]>> {
+  const seoUrlResponse = await invokePost(
+    {
+      address: getSeoUrlEndpoint(),
+      payload: params,
+    },
+    contextInstance
+  );
+
+  return seoUrlResponse.data;
 }
