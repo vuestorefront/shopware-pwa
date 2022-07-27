5a91ae0e83a64bc677afe2c30366d2dbfd7521db
mkucmus <maciejkucmus@gmail.com>
2022-07-27 15:36:09 +0200

    fix(helpers): correct types

--- packages/helpers/src/cms/getCmsLinkTarget.ts
+++ packages/helpers/src/cms/getCmsLinkTarget.ts
@@ -5,7 +5,11 @@ import { CmsSlot } from "@shopware-pwa/commons";
  *
  * @public
  */
-export function getCmsLinkTarget(content?: CmsSlot): String {
+export function getCmsLinkTarget(
+  content?: Omit<CmsSlot, "data"> & {
+    data?: { url?: string; newTab?: boolean };
+  }
+): String {
   const inNewTab = content?.data?.newTab;
 
   return inNewTab ? "_blank" : "_self";
